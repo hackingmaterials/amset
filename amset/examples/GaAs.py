039a5b348a3bf28fc4f4991363b1d15cea78a35f
Alireza <alireza@wustl.edu>
2017-07-30 14:48:59 -0700

    add GaAs AMSET usage example
    Former-commit-id: ae7334cdc98bca5420caacd9a949cde536bb170d [formerly 9d29ca991328649c253ef360fe484dc2e2815fb3] [formerly 694bed35461379536f09523e5977b682e7c5596c [formerly a0e197a185e7e671bc068fa23b9134485819b9a0]]
    Former-commit-id: 9f1e0bb1254d2ac7400999e8dc7c5440b4a5add1 [formerly e3b77152aa92b15bb824b2e98164c649515cba54]
    Former-commit-id: ea2bba250da3081a69011bcec80d1d7aa5516820

---
+++ amset/examples/GaAs.py
@@ -0,0 +1,30 @@
+from amset.amset import AMSET
+import logging
+import os
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+
+    # setting up the inputs:
+    model_params = {"bs_is_isotropic": True, "elastic_scatterings": ["ACD", "IMP", "PIE"],
+                    "inelastic_scatterings": ["POP"]}
+
+    performance_params = {"parallel" : True}
+    GaAs_params = {"epsilon_s": 12.9, "epsilon_inf": 10.9, "W_POP": 8.73, "C_el": 139.7,
+                   "E_D": {"n": 8.6, "p": 8.6}, "P_PIE": 0.052, "scissor": 0.5818}
+    GaAs_path = "../../test_files/GaAs"
+    coeff_file = os.path.join(GaAs_path, "fort.123_GaAs_k23")
+
+
+    AMSET = AMSET(calc_dir=GaAs_path, material_params=GaAs_params,
+        model_params = model_params, performance_params= performance_params,
+        dopings= [-2e15], temperatures=[300, 600, 900])
+
+    # running AMSET
+    AMSET.run(coeff_file=coeff_file)
+
+    # generating files and outputs
+    AMSET.write_input_files()
+    AMSET.to_csv()
+    AMSET.plot(k_plots=['energy'], E_plots='all', show_interactive=True, carrier_types=AMSET.all_types)
+    AMSET.to_json(kgrid=True, trimmed=True, max_ndata=50, nstart=0)
