d4d807093820ab4349584fd22389ac6f79807417
Alireza <alireza@wustl.edu>
2017-10-16 22:33:28 -0700

    added to_file method to start separating analysis from running AMSET (e.g. when we want to plot more things after a long AMSET run had finished)
    Former-commit-id: f4022c89ad03bd8ed44f7f41c61bbfd3fae7935e [formerly 85f994c682ec44f21346aa9363b272130dd10b25] [formerly 463383cc65b5303f2edfce5d97ee2baf4c14f0e6 [formerly 613a5922941fa316f90f1381cc215cab4f3221e8]]
    Former-commit-id: d2a965b4fc714cf6779169134945accba765129a [formerly 4d1160a918fc6eeefe26d2120173b03f8c32198b]
    Former-commit-id: 61c7ad426b67696837190416dff069d3dfbf3589

--- amset/amset.py
+++ amset/amset.py
@@ -108,7 +108,7 @@ class AMSET(object):
 
 
 
-    def run(self, coeff_file, kgrid_tp="coarse"):
+    def run(self, coeff_file, kgrid_tp="coarse", write_outputs=True):
         """
         Function to run AMSET and generate the main outputs.
 
@@ -213,6 +213,9 @@ class AMSET(object):
         if self.e_integration:
             pprint(self.egrid["n"]["mobility"])
             pprint(self.egrid["p"]["mobility"])
+
+        if write_outputs:
+            self.to_file()
 
 
 
@@ -2316,6 +2319,25 @@ class AMSET(object):
         return beta
 
 
+    def to_file(self, dir_path='.', filename='amset_outputs.json',
+                force_write=True):
+        if not force_write:
+            n = 1
+            fname0 = filename
+            while os.path.exists(os.path.join(dir_path, filename)):
+                warnings.warn('The file, {} exists. AMSET outputs will be '
+                        'written in {}'.format(filename, fname0+'_'+str(n)))
+                filename = fname0 + '_' + str(n)
+                n += 1
+
+        # make the output dict
+        out_d = {'kgrid': self.kgrid, 'egrid': self.egrid}
+
+        # write the output dict to file
+        with open(os.path.join(dir_path, filename), 'w') as fp:
+            json.dump(out_d, fp, sort_keys=True, ensure_ascii=False,
+                      cls=MontyEncoder)
+
 
     def to_json(self, kgrid=True, trimmed=False, max_ndata=None, nstart=0):
         """
@@ -3165,14 +3187,15 @@ if __name__ == "__main__":
 
     amset = AMSET(calc_dir=cube_path, material_params=material_params,
                   model_params=model_params, performance_params=performance_params,
-                  dopings = [-3e13],
-                  # temperatures = [300, 600],
-                  temperatures = range(100, 1100, 100),
+                  # dopings = [-3e13],
+                  dopings = [3.32e14],
+                  temperatures = [300],
+                  # temperatures = range(100, 1100, 100),
                   k_integration=False, e_integration=True, fermi_type='e',
                   loglevel=logging.DEBUG
                   )
     profiler = cProfile.Profile()
-    profiler.runcall(lambda: amset.run(coeff_file,kgrid_tp='very fine'))
+    profiler.runcall(lambda: amset.run(coeff_file,kgrid_tp='very coarse', write_outputs=True))
     stats = Stats(profiler, stream=STDOUT)
     stats.strip_dirs()
     stats.sort_stats('cumulative')
