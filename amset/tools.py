838a63c21ef621c925bd112203368d7ae3cbe8c7
Alireza <alireza@wustl.edu>
2017-12-18 16:55:19 -0800

    better "coarse" kgrid_tp to cover intermediate energy values as well
    Former-commit-id: ef72a064b9c00edd1e3b96067bdb5e6188b72465 [formerly 7c4ad5f701469cdfe0d3d95d8d990c1a4e1b0a54] [formerly afaaa7abd3aa1239166603ff019d85c47842b3a5 [formerly 34935004a0c6cf626d2fbdd2e75680b99726dd38]]
    Former-commit-id: 63f2c10dead7117d902e6b82819d9243b2175cda [formerly 1f8d4583121c6ab640666442edf46722d4790d02]
    Former-commit-id: 2eb03caadcd403e3160a16dd940cc1afed0a5a2c

--- amset/tools.py
+++ amset/tools.py
@@ -74,7 +74,7 @@ def generate_k_mesh_axes(important_pts, kgrid_tp='coarse', one_list=True):
                     mesh = [0.004, 0.01, 0.02, 0.03,
                             0.05, 0.07, 0.1, 0.15, 0.25]
                 elif kgrid_tp == 'coarse':
-                    mesh = [0.001, 0.005, 0.01, 0.05, 0.15]
+                    mesh = [0.001, 0.005, 0.01, 0.02, 0.05, 0.15]
                 elif kgrid_tp == 'very coarse':
                     mesh = [0.001, 0.01]
                 else:
