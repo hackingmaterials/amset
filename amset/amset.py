b08fd6f3d3b3cb9b5860e79912de883445efe66b
Jason Frost <jfrost@stanford.edu>
2017-12-19 10:50:55 -0500

    minor bug fix (velocit -> velocity)
    Former-commit-id: fd891d0d0acd754b2ebf1fe6da3e2c150a236cd6 [formerly ac30ca0c0993ad316965da747638f42740c37625] [formerly ca5933bf269d947becc59d240393eaf8543ee9c3 [formerly b92892b18e516730acd12fb6c94a552f82480b39]]
    Former-commit-id: 81167b124c685805a053b140476c0c9b294a7db3 [formerly 5f01a255e9e21c7b3ae3b9067fdb2718cb6b40cf]
    Former-commit-id: b9139bcbb30192ea2f9f61da1a8a48b3012cb013

--- amset/amset.py
+++ amset/amset.py
@@ -1203,7 +1203,7 @@ class AMSET(object):
                                                                            bandgap=self.dft_gap + self.scissor)
 
                     self.kgrid[tp]["energy"][ib][ik] = energy
-                    self.kgrid[tp]["velocity"][ib][ik] = velocit
+                    self.kgrid[tp]["velocity"][ib][ik] = velocity
                     # if tp == 'n':
                     #     print("k_frac = {}".format(self.kgrid['n']["kpoints"][ib][ik]))
                     #     print("k_cart = {}".format(self.kgrid['n']["cartesian kpoints"][ib][ik]))
