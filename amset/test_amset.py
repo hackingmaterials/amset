413871c01b8eb2db2c184a2f7b710418e0719794
Alireza <alireza@wustl.edu>
2018-01-23 19:16:54 -0800

    adjusted tests for calculated fermi (e) at first pass when NOT deleting off k-points

--- amset/test_amset.py
+++ amset/test_amset.py
@@ -66,14 +66,14 @@ class AmsetTest(unittest.TestCase):
 
     def test_GaAs_isotropic_E(self):
         print('\ntesting test_GaAs_isotropic_E...')
-        expected_mu = {'ACD': 52596.9, 'IMP': 156816.3, 'PIE': 111875.4,
-                       'POP': 7704.8, 'overall': 5438.4, 'average': 6093.2}
+        expected_mu = {'ACD': 52617.3, 'IMP': 154824.3, 'PIE': 111864.7,
+                       'POP': 7706.8, 'overall': 5432.5, 'average': 6091.7}
         amset = AMSET(calc_dir=self.GaAs_path, material_params=self.GaAs_params,
                       model_params=self.model_params,
                       performance_params=self.performance_params,
                       dopings=[-2e15], temperatures=[300], k_integration=False,
                       e_integration=True, fermi_type='e',
-                      loglevel=logging.ERROR)
+                      loglevel=logging.DEBUG)
         amset.run(self.GaAs_cube, kgrid_tp='very coarse', write_outputs=False)
         kgrid = amset.kgrid
 
