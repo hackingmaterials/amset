f47bdb24b323fbaf25555fc67d4d10a06598092e
Alireza <alireza@wustl.edu>
2018-03-18 21:35:19 -0700

    fixing the velocity log

--- amset/core.py
+++ amset/core.py
@@ -1757,7 +1757,7 @@ class AMSET(object):
                     self.kgrid[tp]["energy"][ib][ik] = energy
                     self.kgrid[tp]["velocity"][ib][ik] = velocity
                     if tp == 'n':
-                        self.logger.debug('here velocity', velocity)
+                        self.logger.debug('here velocity:\n{}'.format(velocity))
                     # if tp == 'n':
                     #     print("k_frac = {}".format(self.kgrid['n']["kpoints"][ib][ik]))
                     #     print("k_cart = {}".format(self.kgrid['n']["cartesian kpoints"][ib][ik]))
