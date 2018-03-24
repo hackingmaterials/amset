2b2e17868974d5b047be18cb3ed35fda12da32d9
Alireza <alireza@wustl.edu>
2018-03-23 23:00:18 -0700

    add Hartree_to_eV
    Former-commit-id: 4ec7a6f0bc838482ab4181245744742f219e518e [formerly 646111dfcb80b7160bd4420b79527e33886e59bc] [formerly ce611e89f0559303d7f9e878f0d335b186876fed [formerly 29b661057fbe04dcf446486d94c8341a3b5b0fd4]]
    Former-commit-id: 49f1f8f35f2bb4ed524e368e96e4d18e30846aee [formerly 1ea85515c263d3648c03797cce8845be9ac9eab1]
    Former-commit-id: 3c130070cb8abebd441c1bfd13e0aae28503f04c

--- amset/utils/constants.py
+++ amset/utils/constants.py
@@ -5,8 +5,9 @@ from math import pi
 hbar = _cd('Planck constant in eV s') / (2 * pi)
 m_e = _cd('electron mass')  # in kg
 Ry_to_eV = 13.605698066
+Hartree_to_eV = 2.0 * Ry_to_eV
 A_to_m = 1e-10
-m_to_cm = 100.00
+m_to_cm = 100.0
 A_to_nm = 0.1
 e = _cd('elementary charge')
 k_B = _cd("Boltzmann constant in eV/K")
