correct unit conversion for boltztrap2 energies for dos
Former-commit-id: 25cc4888b58484229c3a0bb50afe7f96b6dfc7cb [formerly 7eab774c54620ce80883675d99edb6c294ef68e4] [formerly 7e1d311a25ec7998dca860aba81ff5d23e421c53 [formerly bf66593ce697de0fb5f72d4e9b7877679311aa69]]
Former-commit-id: e14ef090d00945fe5473e8d41e3212c6eb2c1bb1 [formerly 51b60242ea1ecff6d1666608a2284ba03a90b288]
Former-commit-id: cd77b06c23cafca3d5b8ca656378c2d3d1de9e9f

amset/utils/tools.py
@@ -7,8 +7,9 @@ import sys
 import warnings
 
 from amset.utils.analytical_band_from_BZT import Analytical_bands, outer, get_energy
-from amset.utils.constants import hbar, m_e, Ry_to_eV, A_to_m, m_to_cm, A_to_nm, e, k_B,\
-                        epsilon_0, default_small_E, dTdz, sq3
+from amset.utils.constants import hbar, m_e, Ry_to_eV, A_to_m, m_to_cm, \
+    A_to_nm, e, k_B, \
+    epsilon_0, default_small_E, dTdz, sq3, Hartree_to_eV
 from math import pi, log
 
 from pymatgen import Spin
@@ -583,9 +584,9 @@ def get_dos_boltztrap2(params, st, mesh, estep, vbmidx = None, width=0.2, scisso
     w_sum = float(sum(weights))
     weights = [w / w_sum for w in weights]
 
-    fitted = fite.getBands(np.array(ir_kpts), equivalences=equivalences,
+    energies, _, _ = fite.getBands(np.array(ir_kpts), equivalences=equivalences,
                            lattvec=lattvec, coeffs=coeffs)
-    energies = fitted[0]*Ry_to_eV  # shape==(bands, nkpoints)
+    energies *= Hartree_to_eV  # shape==(bands, nkpoints)
     nbands = energies.shape[0]
     if vbmidx:
         ib_start = max(0, vbmidx-4)