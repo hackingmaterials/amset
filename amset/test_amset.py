8b65f55e3ad14a3cfbd6e50942541d12a2608fba
Alireza <alireza@wustl.edu>
2018-03-29 14:18:01 -0700

    update test_GaAs_isotropic_E for 2 valleys rather than Gamma only
    Former-commit-id: 58d4c555382891608aaea9203c6a1d96cf7a3ffe [formerly 6d3da923233337e4d563f8ef3f631350114a3a5e] [formerly 1ec3212ce0afa8fa491305e805a5cd7adf12896c [formerly 19cbd13e7d359cc066e3beef5062c9366d78c6ef]]
    Former-commit-id: 3e0a495de4b3ed6369ae41a5134b1cff32e3a35c [formerly 2466ae0b1765c47a179b592cdb0d4b61bf5521bd]
    Former-commit-id: 4e6b06f7be2714f6c932171629ffca358d1056c2

--- amset/test_amset.py
+++ amset/test_amset.py
@@ -6,7 +6,6 @@ import logging
 import numpy as np
 import os
 import unittest
-
 from amset.core import AMSET
 from amset.utils.tools import rel_diff
 
@@ -18,12 +17,12 @@ class AmsetTest(unittest.TestCase):
                              'elastic_scatterings': ['ACD', 'IMP', 'PIE'],
                              'inelastic_scatterings': ['POP']}
         self.performance_params = {'dE_min': 0.0001, 'nE_min': 2,
-                'BTE_iters': 5,'nkdos':29, 'max_nbands': 1,
+                'BTE_iters': 5,'nkdos':29, 'max_nbands': 1, 'max_nvalleys': 1,
                 'max_normk': 2, 'Ecut': 0.4, 'fermi_kgrid_tp': 'coarse'}
         self.GaAs_params = {'epsilon_s': 12.9, 'epsilon_inf': 10.9,
                 'W_POP': 8.73, 'C_el': 139.7, 'E_D': {'n': 8.6, 'p': 8.6},
-                'P_PIE': 0.052, 'scissor': 0.5818,
-                'important_points':{tp:[[0.0, 0.0, 0.0]] for tp in ['p', 'n']}}
+                'P_PIE': 0.052, 'scissor': 0.5818}
+                # 'important_points':{tp:[[0.0, 0.0, 0.0]] for tp in ['p', 'n']}}
         self.GaAs_path = os.path.join(test_dir, '..', 'test_files', 'GaAs')
         # self.GaAs_cube = os.path.join(self.GaAs_path, "fort.123_GaAs_1099kp")
         self.GaAs_cube = os.path.join(self.GaAs_path, "nscf-uniform/boltztrap/fort.123")
@@ -67,10 +66,11 @@ class AmsetTest(unittest.TestCase):
     def test_GaAs_isotropic_E(self):
         print('\ntesting test_GaAs_isotropic_E...')
         # w/ /sq3 factor
-        expected_mu = {'ACD': 93991.3170, 'IMP': 117744.7036, 'PIE':192754.20732,
-                       'POP': 7236.73298, 'overall':4524.97423, 'average': 6153.69136}
+        expected_mu = {'ACD': 78610.06963, 'IMP': 289336.2522, 'PIE':193117.5001,
+                       'POP': 7209.6673, 'overall':5946.595, 'average': 6247.7331}
 
-
+        performance_params = self.performance_params.copy()
+        performance_params['max_nvalleys'] = 2
         amset = AMSET(calc_dir=self.GaAs_path, material_params=self.GaAs_params,
                       model_params=self.model_params,
                       performance_params=self.performance_params,
