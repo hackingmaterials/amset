38bc8cb1a9a7ae0d2676b236c35a90bce0a7559d
Alireza <alireza@wustl.edu>
2018-03-29 10:23:19 -0700

    small cleanup

--- amset/test_amset.py
+++ amset/test_amset.py
@@ -1,11 +1,8 @@
 # coding: utf-8
 
-from __future__ import unicode_literals
-from __future__ import absolute_import
+from __future__ import unicode_literals, absolute_import
 import json
 import logging
-from copy import deepcopy
-
 import numpy as np
 import os
 import unittest
