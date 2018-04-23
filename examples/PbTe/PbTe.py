a05d92c5c724b49cbd1ca0bc856b8a96224c275a
Alireza <alireza@wustl.edu>
2018-04-23 15:33:48 -0700

    set to only one valley and band for now with the correct fort.123 file
    Former-commit-id: 774b1d4b0885608f9a41ec3b214f3d7315dca1ff [formerly 1dda5b0288871dd9ee2d55b8574605ff562adb15] [formerly eeace2794aa00748c7072c9eac208b243be628ff [formerly 1656f39054ffac4bde9563a77372d23d0b46a89c]]
    Former-commit-id: d054db6be55d6c786054d03385d62022a95a66b9 [formerly 046e4194e7e3c1cb712254dde62f57471ae53e30]
    Former-commit-id: cba5035cac2f4912a7296411959a885dd8ba5ec5

--- examples/PbTe/PbTe.py
+++ examples/PbTe/PbTe.py
@@ -18,16 +18,19 @@ if __name__ == "__main__":
                   vasprun_file=os.path.join(PbTe_dir, "vasprun.xml"),
                   material_params=material_params,
                   model_params = model_params,
-                  dopings= [-2e15],
+                  performance_params={"max_nbands": 1, "max_nvalleys": 1},
+                  dopings= [-1e19, 1e19],
                   temperatures=[300, 600])
 
     # running AMSET
-    AMSET.run(coeff_file=coeff_file, kgrid_tp="very coarse")
+    AMSET.run(coeff_file=coeff_file, kgrid_tp="coarse")
 
     # generating files and outputs
     AMSET.write_input_files()
     AMSET.to_csv()
-    AMSET.plot(k_plots=['energy'], E_plots='all', show_interactive=True,
-               carrier_types=AMSET.all_types, save_format=None)
+    AMSET.plot(k_plots=['energy'], E_plots=['energy', 'velocity', 'ACD'],
+               show_interactive=True,
+               carrier_types=AMSET.all_types,
+               save_format=None)
     AMSET.to_file()
     AMSET.to_json(kgrid=True, trimmed=True, max_ndata=50, nstart=0)
