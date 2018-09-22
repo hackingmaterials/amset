d702e42566a21b7958fdc419ad6adf1f53a4d487
Alireza <alireza.faghaninia@gmail.com>
2018-09-21 19:30:28 -0700

    nicely plot all the peaks found overlay on the line mode bs plot
    Former-commit-id: 42b0dd005cb3c2573f0616b00ac5dc5e5d3d81e9 [formerly ef17122c589646402ed196c1e24ec6529fb36f97] [formerly 0a14bd77b1135756b2ef8421157fe8d57897f32f [formerly b93f0db9218cee3bd3a4b0039903f48da0866f4b]] [formerly 90fe427053dfb87fe6b928a579d9d548ba5d4afe [formerly 718b1a39e8db5c80cde54b7df9164c6968ce3ff2] [formerly d01a80ee56b1497cc41d8d7ed08684ea40024278 [formerly 2e9c45ff82631ba362fbb79d012ca1c0e2f7bf80]]]
    Former-commit-id: 82a0be9486bf14726fbed692baa0a980202626ab [formerly d191b79ce0950e7880068e5b6b1a3996c81a7235] [formerly 9f2e6be37b5cf5611deb8cd8c7a470b93656336c [formerly 1c1a2bdba464054da8b34f6d0896c7087e8e9647]]
    Former-commit-id: 236174b8cf01fbd480686f7a2282c9eba7a38695 [formerly a3caaac688473898300b01ea7d62ef6703cfd4df]
    Former-commit-id: 3a222119b9f7936da4088c4a626e6f212958359b

--- amset/plot_bs_with_annotation.py
+++ amset/plot_bs_with_annotation.py
@@ -1,13 +1,14 @@
 from amset.core import Amset
 from amset.utils.constants import A_to_nm
-from amset.utils.tools import get_bindex_bspin, interpolate_bs, get_energy_args
+from amset.utils.tools import get_bindex_bspin, interpolate_bs, \
+    get_energy_args, kpts_to_first_BZ
 from matminer import PlotlyFig
 import numpy as np
 import os
 import pandas as pd
 from pymatgen.io.vasp import Vasprun, Spin
 from pymatgen.symmetry.bandstructure import HighSymmKpath
-from scipy.signal import find_peaks_cwt
+import plotly.graph_objs as go
 
 abs_dir = os.path.dirname(__file__)
 
@@ -17,30 +18,44 @@ help easily detect the important pockets. It can be used to visually test
 the output of get_bs_extrema function.
 """
 
-# vrun_file = os.path.join(abs_dir, "../test_files/GaAs/28_electrons_line/vasprun.xml")
 vrun_file = os.path.join(abs_dir, "../test_files/GaAs/nscf-uniform/vasprun.xml")
 coeff_file = os.path.join(abs_dir, "../test_files/GaAs/nscf-uniform/fort.123")
 
-# vrun_file = os.path.join(abs_dir, "../test_files/Si/vasprun.xml")
-# coeff_file = os.path.join(abs_dir, "../test_files/Si/Si_fort.123")
-#
-# vrun_file = os.path.join(abs_dir, "../test_files/PbTe/vasprun.xml")
-# coeff_file = os.path.join(abs_dir, "../test_files/PbTe/fort.123")
+vrun_file = os.path.join(abs_dir, "../test_files/Si/vasprun.xml")
+coeff_file = os.path.join(abs_dir, "../test_files/Si/Si_fort.123")
 
-amset = Amset(calc_dir='.', material_params={'epsilon_s': 12.9})
+vrun_file = os.path.join(abs_dir, "../test_files/PbTe/vasprun.xml")
+coeff_file = os.path.join(abs_dir, "../test_files/PbTe/fort.123")
+
+# vrun_file = os.path.join(abs_dir, "../test_files/InP_mp-20351/vasprun.xml")
+# coeff_file = os.path.join(abs_dir, "../test_files/InP_mp-20351/fort.123")
+
+# vrun_file = '/Users/alirezafaghaninia/Documents/amset_examples/ZnO/vasprun.xml'
+# coeff_file = '/Users/alirezafaghaninia/Documents/amset_examples/ZnO/boltztrap/fort.123'
+
+LINE_DENSITY = 30
+
+
+amset = Amset(calc_dir='.', material_params={'epsilon_s': 12.9}, temperatures=[300, 600, 1000], dopings= [-1e20, -1e21, 1e20, 1e21])
 amset.read_vrun(vasprun_file=vrun_file)
-# extrema = amset.get_bs_extrema(bs=self.GaAs_vrun.get_band_structure(),
-#             coeff_file=self.GaAs_cube, nbelow_vbm=0, nabove_cbm=0)
+
+amset.update_cbm_vbm_dos(coeff_file)
+extrema = amset.find_all_important_points(coeff_file, nbelow_vbm=0, nabove_cbm=0,
+                                  interpolation="boltztrap1", line_density=LINE_DENSITY,
+                                          )
 
 
 vrun = Vasprun(vrun_file)
 hsk = HighSymmKpath(vrun.final_structure)
-hs_kpoints , _ = hsk.get_kpoints(line_density=30)
+hs_kpoints , _ = hsk.get_kpoints(line_density=LINE_DENSITY)
+hs_kpoints = kpts_to_first_BZ(hs_kpoints)
 
 bs = vrun.get_band_structure()
 bsd = {}
 bsd['kpoints'] = hs_kpoints
 hs_kpoints = np.array(hs_kpoints)
+
+
 
 bsd['str_kpts'] = [str(k) for k in bsd['kpoints']]
 bsd['cartesian kpoints (1/nm)'] = [amset.get_cartesian_coords(k)/A_to_nm for k in bsd['kpoints']]
@@ -65,11 +80,46 @@ bsd['cb0'], _, _ = interpolate_bs(bsd['kpoints'], interp_params, iband=1,
 bsd['cb0'] -= vbm
 cbm = min(bsd['cb0'])
 
-# extrema_idx = find_peaks_cwt(bsd['cb0'], widths=np.arange(0.03, 0.2, 0.02))
-
 bs_df = pd.DataFrame.from_dict(bsd)
 
 pf = PlotlyFig(bs_df, x_title='index', y_title='Energy (eV)')
-pf.xy([(bs_df.index, 'vb0'), (bs_df.index, 'cb0')], labels='str_kpts')
+plt = pf.xy([(bs_df.index, 'vb0'), (bs_df.index, 'cb0')], labels='str_kpts', return_plot=True)
 # pf.xy([(bs_df.index, 'vb0'), (bs_df.index, 'cb0')], labels='normk')
 
+extrema_data_x = []
+extrema_data_y = []
+extrema_data_labels = []
+
+for iband, tp in enumerate(['p', 'n']):
+    energies, _, _ = interpolate_bs(extrema[tp], interp_params, iband=iband,
+                                      method="boltztrap1", scissor=0.0,
+                                      matrix=vrun.final_structure.lattice.matrix,
+                                      n_jobs=-1)
+    for k in extrema[tp]:
+        idx = np.argmin([np.linalg.norm(hs_kpoints-k, axis=1)])
+        extrema_data_x.append(idx)
+    extrema_data_labels += [str(k.tolist()) for k in extrema[tp]]
+    extrema_data_y += list(energies - vbm)
+
+word_map = {'p': 'vb0', 'n': 'cb0'}
+for idx in bs_df.index:
+    for tp in ['p', 'n']:
+        if bs_df['str_kpts'][idx] in extrema[tp]:
+            extrema_data_x.append(idx)
+            extrema_data_y.append(bs_df[word_map[tp]][idx])
+            extrema_data_labels.append(bs_df['str_kpts'][idx])
+
+
+plt['data'].append(
+go.Scatter(
+    x = extrema_data_x,
+    y = extrema_data_y,
+    mode = 'markers',
+    name = 'extrema',
+    text = extrema_data_labels,
+    marker = {'size': 15, 'symbol': 'diamond-open', 'color': 'rgb(0, 255, 0)',
+              'line': {'width': 3}}
+)
+)
+
+pf.create_plot(plt)
