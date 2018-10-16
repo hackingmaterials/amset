09457a303c1b5ea5e44e26c49ee2fcb8d0e4014e
Alireza <alireza.faghaninia@gmail.com>
2018-10-16 14:01:16 -0700

    tidy up plot_bs_with_annotation a bit
    Former-commit-id: 14b593972d4b889dc72e468064d20a23ea5fe89c [formerly 69ae8ccec440b6fec68060eba1392dd52abda41e] [formerly 348d4dc0f85817cae67cbf9c6d8a3771d0d70811 [formerly d12f8e9ac2c2f1d41e0b0f9c40506b58b6656d4d]] [formerly bf54d7a7f6997bd93ab27ae0ebbdb9c83d6dbc5e [formerly 6e6e0151358c0336b45fd7c5d75be7714b717e7d] [formerly 96762b33d284a24b21adc192eb38d047ae89dfb0 [formerly 31193f92b3b2b096bf2d3815ce7af696a13f5351]]]
    Former-commit-id: d6f3b283e42d23a00a22bbf571ddb20f3b885ce5 [formerly 32cbfd1cef425189b49e341046f9eb59e628ffbd] [formerly 90247cd38a0d70d36e1c7142124b1d9b6ad42256 [formerly e77ddc38cf422d29d23017db970b20f516a2efc8]]
    Former-commit-id: e94ccb140b6cd57992def6a4b5239404ac6b46e0 [formerly 056a30b5a215ef51f35b069fa7d80e4d49854b8b]
    Former-commit-id: 74f1a26fe4f960297e000aadf0a6c63257ecd2b5

--- amset/plot_bs_with_annotation.py
+++ amset/plot_bs_with_annotation.py
@@ -9,6 +9,8 @@ import pandas as pd
 from pymatgen.io.vasp import Vasprun, Spin
 from pymatgen.symmetry.bandstructure import HighSymmKpath
 import plotly.graph_objs as go
+from thermoelectrics_work.amset_examples.material_params import global_dopings, \
+    global_temperatures
 
 abs_dir = os.path.dirname(__file__)
 
@@ -18,108 +20,115 @@ help easily detect the important pockets. It can be used to visually test
 the output of get_bs_extrema function.
 """
 
-vrun_file = os.path.join(abs_dir, "../test_files/GaAs/nscf-uniform/vasprun.xml")
-coeff_file = os.path.join(abs_dir, "../test_files/GaAs/nscf-uniform/fort.123")
+comps = {
+    'GaAs': 'GaAs_mp-2534',
+    'Si': 'Si_mp-149',
+    'PbTe': 'PbTe_mp-19717',
+    'InP': 'InP_mp-20351',
+    'AlCuS2': 'AlCuS2_mp-4979',
+    'In2O3': 'In2O3_mp-22598',
+}
 
-vrun_file = os.path.join(abs_dir, "../test_files/Si/vasprun.xml")
-coeff_file = os.path.join(abs_dir, "../test_files/Si/Si_fort.123")
-
-vrun_file = os.path.join(abs_dir, "../test_files/PbTe/vasprun.xml")
-coeff_file = os.path.join(abs_dir, "../test_files/PbTe/fort.123")
-
-# vrun_file = os.path.join(abs_dir, "../test_files/InP_mp-20351/vasprun.xml")
-# coeff_file = os.path.join(abs_dir, "../test_files/InP_mp-20351/fort.123")
-
-# vrun_file = '/Users/alirezafaghaninia/Documents/amset_examples/ZnO/vasprun.xml'
-# coeff_file = '/Users/alirezafaghaninia/Documents/amset_examples/ZnO/boltztrap/fort.123'
-
+# INPUTS
+COMPOUND = 'GaAs'
 LINE_DENSITY = 30
 
 
-amset = Amset(calc_dir='.', material_params={'epsilon_s': 12.9}, temperatures=[300, 600, 1000], dopings= [-1e20, -1e21, 1e20, 1e21])
-amset.read_vrun(vasprun_file=vrun_file)
+if __name__ == "__main__":
+    vrun_file = os.path.join(abs_dir, "../test_files/{}/vasprun.xml".format(comps[COMPOUND]))
+    coeff_file = os.path.join(abs_dir, "../test_files/{}/fort.123".format(comps[COMPOUND]))
+    vrun = Vasprun(vrun_file)
+    formula = vrun.final_structure.composition.reduced_formula
 
-amset.update_cbm_vbm_dos(coeff_file)
-extrema = amset.find_all_important_points(coeff_file, nbelow_vbm=0, nabove_cbm=0,
-                                  interpolation="boltztrap1", line_density=LINE_DENSITY,
-                                          )
+    dopings = [1e20, -1e20]
+    temperatures = global_temperatures[formula]
+
+    amset = Amset(calc_dir='.', material_params={'epsilon_s': 12.9},
+              temperatures=temperatures, dopings=dopings,
+              performance_params={'Ecut': 1.0}
+                  )
+    amset.read_vrun(vasprun_file=vrun_file)
+    amset.update_cbm_vbm_dos(coeff_file)
+    extrema = amset.find_all_important_points(coeff_file,
+                                              nbelow_vbm=0,
+                                              nabove_cbm=0,
+                                              interpolation="boltztrap1",
+                                              line_density=LINE_DENSITY
+                                              )
 
 
-vrun = Vasprun(vrun_file)
-hsk = HighSymmKpath(vrun.final_structure)
-hs_kpoints , _ = hsk.get_kpoints(line_density=LINE_DENSITY)
-hs_kpoints = kpts_to_first_BZ(hs_kpoints)
+    hsk = HighSymmKpath(vrun.final_structure)
+    hs_kpoints , _ = hsk.get_kpoints(line_density=LINE_DENSITY)
+    hs_kpoints = kpts_to_first_BZ(hs_kpoints)
 
-bs = vrun.get_band_structure()
-bsd = {}
-bsd['kpoints'] = hs_kpoints
-hs_kpoints = np.array(hs_kpoints)
+    bs = vrun.get_band_structure()
+    bsd = {}
+    bsd['kpoints'] = hs_kpoints
+    hs_kpoints = np.array(hs_kpoints)
 
+    bsd['str_kpts'] = [str(k) for k in bsd['kpoints']]
+    bsd['cartesian kpoints (1/nm)'] = [amset.get_cartesian_coords(k)/A_to_nm for k in bsd['kpoints']]
+    bsd['normk'] = np.linalg.norm(bsd['cartesian kpoints (1/nm)'], axis=1)
 
+    cbm_idx, cbm_spin = get_bindex_bspin(bs.get_cbm(), is_cbm=True)
+    vbmd = bs.get_vbm()
+    vbm_idx, vbm_spin = get_bindex_bspin(vbmd, is_cbm=False)
+    vbm = vbmd['energy']
+    print(np.array(bs.bands[Spin.up]).shape)
 
-bsd['str_kpts'] = [str(k) for k in bsd['kpoints']]
-bsd['cartesian kpoints (1/nm)'] = [amset.get_cartesian_coords(k)/A_to_nm for k in bsd['kpoints']]
-bsd['normk'] = np.linalg.norm(bsd['cartesian kpoints (1/nm)'], axis=1)
+    interp_params = get_energy_args(coeff_file, ibands=[vbm_idx+1, vbm_idx+2])
+    # bsd['vb0'] = bs.bands[vbm_spin][vbm_idx] - vbm
+    bsd['vb0'], _, _ = interpolate_bs(bsd['kpoints'], interp_params, iband=0,
+                           method="boltztrap1", scissor=0.0, matrix=vrun.final_structure.lattice.matrix, n_jobs=-1)
+    vbm = max(bsd['vb0'])
+    bsd['vb0'] -= vbm
 
-cbm_idx, cbm_spin = get_bindex_bspin(bs.get_cbm(), is_cbm=True)
-vbmd = bs.get_vbm()
-vbm_idx, vbm_spin = get_bindex_bspin(vbmd, is_cbm=False)
-vbm = vbmd['energy']
-print(np.array(bs.bands[Spin.up]).shape)
+    # bsd['cb0'] = bs.bands[cbm_spin][cbm_idx] - vbm
+    bsd['cb0'], _, _ = interpolate_bs(bsd['kpoints'], interp_params, iband=1,
+                           method="boltztrap1", scissor=0.0, matrix=vrun.final_structure.lattice.matrix, n_jobs=-1)
+    bsd['cb0'] -= vbm
+    cbm = min(bsd['cb0'])
 
-interp_params = get_energy_args(coeff_file, ibands=[vbm_idx+1, vbm_idx+2])
-# bsd['vb0'] = bs.bands[vbm_spin][vbm_idx] - vbm
-bsd['vb0'], _, _ = interpolate_bs(bsd['kpoints'], interp_params, iband=0,
-                       method="boltztrap1", scissor=0.0, matrix=vrun.final_structure.lattice.matrix, n_jobs=-1)
-vbm = max(bsd['vb0'])
-bsd['vb0'] -= vbm
+    bs_df = pd.DataFrame.from_dict(bsd)
 
-# bsd['cb0'] = bs.bands[cbm_spin][cbm_idx] - vbm
-bsd['cb0'], _, _ = interpolate_bs(bsd['kpoints'], interp_params, iband=1,
-                       method="boltztrap1", scissor=0.0, matrix=vrun.final_structure.lattice.matrix, n_jobs=-1)
-bsd['cb0'] -= vbm
-cbm = min(bsd['cb0'])
+    pf = PlotlyFig(bs_df, x_title='index', y_title='Energy (eV)')
+    plt = pf.xy([(bs_df.index, 'vb0'), (bs_df.index, 'cb0')], labels='str_kpts', return_plot=True)
+    # pf.xy([(bs_df.index, 'vb0'), (bs_df.index, 'cb0')], labels='normk')
 
-bs_df = pd.DataFrame.from_dict(bsd)
+    extrema_data_x = []
+    extrema_data_y = []
+    extrema_data_labels = []
 
-pf = PlotlyFig(bs_df, x_title='index', y_title='Energy (eV)')
-plt = pf.xy([(bs_df.index, 'vb0'), (bs_df.index, 'cb0')], labels='str_kpts', return_plot=True)
-# pf.xy([(bs_df.index, 'vb0'), (bs_df.index, 'cb0')], labels='normk')
-
-extrema_data_x = []
-extrema_data_y = []
-extrema_data_labels = []
-
-for iband, tp in enumerate(['p', 'n']):
-    energies, _, _ = interpolate_bs(extrema[tp], interp_params, iband=iband,
-                                      method="boltztrap1", scissor=0.0,
-                                      matrix=vrun.final_structure.lattice.matrix,
-                                      n_jobs=-1)
-    for k in extrema[tp]:
-        idx = np.argmin([np.linalg.norm(hs_kpoints-k, axis=1)])
-        extrema_data_x.append(idx)
-    extrema_data_labels += [str(k.tolist()) for k in extrema[tp]]
-    extrema_data_y += list(energies - vbm)
-
-word_map = {'p': 'vb0', 'n': 'cb0'}
-for idx in bs_df.index:
-    for tp in ['p', 'n']:
-        if bs_df['str_kpts'][idx] in extrema[tp]:
+    for iband, tp in enumerate(['p', 'n']):
+        energies, _, _ = interpolate_bs(extrema[tp], interp_params, iband=iband,
+                                          method="boltztrap1", scissor=0.0,
+                                          matrix=vrun.final_structure.lattice.matrix,
+                                          n_jobs=-1)
+        for k in extrema[tp]:
+            idx = np.argmin([np.linalg.norm(hs_kpoints-k, axis=1)])
             extrema_data_x.append(idx)
-            extrema_data_y.append(bs_df[word_map[tp]][idx])
-            extrema_data_labels.append(bs_df['str_kpts'][idx])
+        extrema_data_labels += [str(k.tolist()) for k in extrema[tp]]
+        extrema_data_y += list(energies - vbm)
+
+    word_map = {'p': 'vb0', 'n': 'cb0'}
+    for idx in bs_df.index:
+        for tp in ['p', 'n']:
+            if bs_df['str_kpts'][idx] in extrema[tp]:
+                extrema_data_x.append(idx)
+                extrema_data_y.append(bs_df[word_map[tp]][idx])
+                extrema_data_labels.append(bs_df['str_kpts'][idx])
 
 
-plt['data'].append(
-go.Scatter(
-    x = extrema_data_x,
-    y = extrema_data_y,
-    mode = 'markers',
-    name = 'extrema',
-    text = extrema_data_labels,
-    marker = {'size': 15, 'symbol': 'diamond-open', 'color': 'rgb(0, 255, 0)',
-              'line': {'width': 3}}
-)
-)
+    plt['data'].append(
+    go.Scatter(
+        x = extrema_data_x,
+        y = extrema_data_y,
+        mode = 'markers',
+        name = 'extrema',
+        text = extrema_data_labels,
+        marker = {'size': 15, 'symbol': 'diamond-open', 'color': 'rgb(0, 255, 0)',
+                  'line': {'width': 3}}
+    )
+    )
 
-pf.create_plot(plt)
+    pf.create_plot(plt)
