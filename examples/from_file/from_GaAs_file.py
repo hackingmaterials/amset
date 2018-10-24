f31a2403a1a32caf8cbe0931a42f48d60608b687
Alireza <alireza.faghaninia@gmail.com>
2018-10-23 23:28:57 -0700

    update from_file example for more plots

--- examples/from_file/from_GaAs_file.py
+++ examples/from_file/from_GaAs_file.py
@@ -13,5 +13,7 @@ if __name__ == "__main__":
     amset = Amset.from_file(path=os.path.join("..", "GaAs", "run_data"),
                             filename="amsetrun.json")
 
-    amset.plot(k_plots=['energy'], E_plots=['velocity', 'ACD', 'g_th'], show_interactive=True,
-               carrier_types=amset.all_types, save_format=None)
+    amset.plot(k_plots=['energy', 'velocity'],
+               E_plots=['velocity', 'ACD', 'S_i', 'S_o']+amset.elastic_scats,
+               show_interactive=True,
+               carrier_types=amset.all_types)
