0dd9cc793eb924479c1b90ff4a0146fbdf91679c
Alireza <alireza.faghaninia@gmail.com>
2018-11-02 16:31:06 -0700

    timeout --> timeout_hours

--- amset/core.py
+++ amset/core.py
@@ -166,10 +166,10 @@ class Amset(object):
             properties in the energy-scale is supported
         loglevel (int): e.g. logging.DEBUG; set logging.ERROR to turn off
             the logging
-        timeout (float): timeout in hours. If Amset takes longer than this,
+        timeout_hours (float): timeout_hours in hours. If Amset takes longer than this,
             the calculations will stop. However, if transport properties
             are already calculated, the postprocessing (e.g. write to file)
-            might violate this timeout.
+            might violate this timeout_hours.
 
     Returns:
         (None): results are accessible through various methods such as the
@@ -189,7 +189,7 @@ class Amset(object):
     def __init__(self, calc_dir, material_params, vasprun_file=None,
                  model_params=None, performance_params=None,
                  dopings=None, temperatures=None, integration='e',
-                 loglevel=None, timeout=48):
+                 loglevel=None, timeout_hours=48):
         self.logger = setup_custom_logger(
                 'amset_logger', calc_dir, 'amset.log', level=loglevel)
         self.calc_dir = calc_dir
@@ -218,7 +218,7 @@ class Amset(object):
         self.offset_from_vrun = {'n': 0.0, 'p': 0.0}
         self.kgrid_tp = None
         self.seebeck = {'n': None, 'p': None}
-        self.timeout = timeout * 3600.0
+        self.timeout_hours = timeout_hours * 3600.0
         self.start_time = time.time()
 
 
@@ -239,10 +239,10 @@ class Amset(object):
         stats.print_stats(nfuncs)
 
 
-    def _check_timeout(self):
-        if time.time() - self.start_time > self.timeout:
+    def _check_timeout_hours(self):
+        if time.time() - self.start_time > self.timeout_hours:
             raise AmsetError(self.logger, 'The calculations exceeded the '
-                        'timeout of {:.4f} hours'.format(self.timeout/3600.0))
+                        'timeout_hours of {:.4f} hours'.format(self.timeout_hours/3600.0))
 
 
     def run(self, coeff_file=None, kgrid_tp="coarse"):
@@ -286,7 +286,7 @@ class Amset(object):
                              'on the interpolation method does not match with '
                              'the input bandgap! {}!={}'.format(gap, gap_orig))
 
-        self._check_timeout()
+        self._check_timeout_hours()
         if self.integration == 'k':
             kpts = self.generate_kmesh(important_points={'n': [[0.0, 0.0, 0.0]],
                                                          'p': [[0.0, 0.0, 0.0]]},
@@ -343,7 +343,7 @@ class Amset(object):
         self.denominator = {c: {T: {'p': 0.0, 'n': 0.0} for T in self.temperatures} for c in self.dopings}
         self.seeb_denom = {c: {T: {'p': 0.0, 'n': 0.0} for T in self.temperatures} for c in self.dopings}
         for self.ibrun, (self.nbelow_vbm, self.nabove_cbm) in enumerate(ibands_tuple):
-            self._check_timeout()
+            self._check_timeout_hours()
             self.logger.info('going over conduction and valence # {}'.format(self.ibrun))
             self.all_important_pts = self.find_all_important_points(coeff_file,
                                            nbelow_vbm=self.nbelow_vbm,
@@ -358,7 +358,7 @@ class Amset(object):
                                    min(len(self.important_pts['p']), self.max_nvalleys["p"] or 1000))
 
             for ivalley in range(max_nvalleys):
-                self._check_timeout()
+                self._check_timeout_hours()
                 self.count_mobility[self.ibrun] = self.count_mobility0[self.ibrun]
                 important_points = {'n': None, 'p': None}
                 once_called = True
@@ -410,7 +410,7 @@ class Amset(object):
                                                      nbelow_vbm=self.nbelow_vbm,
                                                      nabove_cbm=self.nabove_cbm,
                                                      num_bands={'p': 1, 'n': 1})
-                self._check_timeout()
+                self._check_timeout_hours()
 
                 if min(energies['n']) - self.cbm_vbm['n']['energy'] > self.Ecut['n']:
                     self.logger.info('not counting conduction band {} valley\
@@ -892,7 +892,7 @@ class Amset(object):
                                 iband=iband, sgn=sgn, method=self.interpolation,
                                 scissor=self.scissor, return_mass=False,
                                 matrix=self._vrun.lattice.matrix, n_jobs=self.n_jobs)
-                        self._check_timeout()
+                        self._check_timeout_hours()
                     if self.integration == 'k':
                         self.energy_array[tp].append(
                             self.grid_from_ordered_list(energies[tp], tp, none_missing=True))
@@ -2758,7 +2758,7 @@ class Amset(object):
 
         # solve BTE to calculate S_i scattering rate and perturbation (g) in an iterative manner
         for iter in range(self.BTE_iters):
-            self._check_timeout()
+            self._check_timeout_hours()
             self.logger.info("Performing iteration # {}".format(iter))
             if "POP" in self.inelastic_scats:
                 if self.bs_is_isotropic:
