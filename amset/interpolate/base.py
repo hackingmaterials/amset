5418c41a78d34ab421ac2292a2fbd77b3b0e053a
Alex Ganose <alexganose@googlemail.com>
2019-01-31 15:28:10 -0800

    WIP: Add option to get DOS from interpolater
    Former-commit-id: 07bcfd6aaa4f94e1bb63ee3bcfbbc921dcfde8cf [formerly 15aa550bfd5ca6471204b68213cbc4f649b4eb0e] [formerly 022ca5577a0649a962073746a325721fe53e03cc [formerly 15e4ad0d4911d02536f52b5727ed02f288d380d4]] [formerly 473caedcb42c66ef5912c03f5d2862384157d138 [formerly 8465209f935068c6b78a7711ef38bbf153d193b6] [formerly 079257b00dd4640175454e9cc8efde94bda6ffa4 [formerly f9302ee4296d3b1c6783e6fabc5eb62a0c8cecac]]]
    Former-commit-id: f40c4b6ada1dbfd1cb46eb6f3a342ef46dd123c8 [formerly 4ef6a5342eca107de7ae9df37451c503532724cd] [formerly 98da327ec8971f454edc43634e459448e6feab6d [formerly 800c52a4937f9e4e62c3998eaa30567f8f96e5f9]]
    Former-commit-id: ffb4728bda3f568e9e3d715c976a8845b355fc29 [formerly 23f441c9f879f8419eeee57abd0604f0a9b0b8bf]
    Former-commit-id: 84486124790d165c744f6b919c1697121de8075d

--- amset/interpolate/base.py
+++ amset/interpolate/base.py
@@ -1,61 +1,123 @@
+import numpy as np
+
 from abc import ABC, abstractmethod
+from logging import Logger
+from typing import Optional, Union, Tuple, List
+
 from monty.json import MSONable
 from amset.logging import LoggableMixin
+from pymatgen.electronic_structure.bandstructure import BandStructure
+from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
 
 
 class AbstractInterpolater(MSONable, LoggableMixin, ABC):
 
-    def __init__(self, band_structure, num_electrons, calc_dir='.',
-                 logger=True, log_level=None):
+    def __init__(self, band_structure: BandStructure, num_electrons: int,
+                 calc_dir: str = '.', logger: Union[bool, Logger] = True,
+                 log_level: Optional[int] = None, symprec: float = 0.1):
         self._band_structure = band_structure
         self._num_electrons = num_electrons
         self._calc_dir = calc_dir
         self._logger = self.get_logger(logger, level=log_level)
+        self._symprec = symprec
+        self._sga = SpacegroupAnalyzer(band_structure.structure,
+                                       symprec=symprec)
 
     @abstractmethod
     def initialize(self):
         """Initialise the interpolater."""
         pass
 
-    def get_energy(self, kpoint, iband, return_velocity=True,
-                   return_effective_mass=False):
+    @abstractmethod
+    def get_energy(self, kpoint: np.ndarray, iband: int,
+                   return_velocity: bool = False,
+                   return_effective_mass: bool = False
+                   ) -> Union[float, Tuple[Union[float, np.ndarray], ...]]:
         """Gets the interpolated energy for a specific k-point and band.
 
         Args:
-            kpoint (np.ndarray): The k-point fractional coordinates.
-            iband (int): The band index (1-indexed).
-            return_velocity (bool, optional): Whether to return the band
-                velocity.
-            return_effective_mass (bool, optional): Whether to return the band
-                effective mass.
+            kpoint: The k-point fractional coordinates.
+            iband: The band index (0-indexed).
+            return_velocity: Whether to return the band velocity.
+            return_effective_mass: Whether to return the band effective mass.
 
         Returns:
-            (tuple[int or np.ndarray]): A tuple containing the band energy, and
-            optionally the velocity and effective mass if asked for. The
-            velocities and effective masses are given as the full 3x3 tensors
-            along cartesian directions.
+            The band energies as a numpy array. If ``return_velocity`` or
+            ``return_effective_mass`` are ``True`` a tuple is returned,
+            formatted as::
+
+               (energy, Optional[velocity], Optional[effecitve_mass])
+
+            The velocity and effective mass are given as the 1x3 trace and
+            full 3x3 tensor, respectively (along cartesian directions).
         """
-        pass
 
     @abstractmethod
-    def get_energies(self, kpoints, iband, scissor=0.0, is_cb=None,
-                     return_effective_mass=True):
+    def get_energies(self, kpoints: np.ndarray,
+                     iband: Optional[Union[int, List[int]]] = None,
+                     scissor: float = 0.0,
+                     return_velocity: bool = False,
+                     return_effective_mass: bool = False
+                     ) -> Union[np.ndarray, Tuple[np.ndarray]]:
         """Gets the interpolated energies for multiple k-points in a band.
 
         Args:
-            kpoints (np.ndarray): The k-points in fractional coordinates.
-            iband (int): The band index (1-indexed).
-            scissor (float, optional): The amount by which the band gap is
-                scissored.
-            is_cb (bool, optional): Whether the band of interest is a conduction
-                band. Ignored if ``scissor == 0``.
-            return_effective_mass (bool, optional): Whether to return the band
-                effective masses.
+            kpoints: The k-points in fractional coordinates.
+            iband: A band index or list of band indicies for which to get the
+                energies. Band indices are 0-indexed. If ``None``, the energies
+                for all available bands will be returned.
+            scissor: The amount by which the band gap is scissored.
+            return_velocity: Whether to return the band velocities.
+            return_effective_mass: Whether to return the band effective masses.
 
         Returns:
-            (tuple[np.ndarray]): A tuple containing the band energies,
-             velocities and, optionally, the effective masses if asked for, for
-             each k-point. The velocities and effective masses are given as
-             the full 3x3 tensors along cartesian directions.
+            The band energies as a numpy array. If ``return_velocity`` or
+            ``return_effective_mass`` are ``True`` a tuple is returned,
+            formatted as::
+
+                (energies, Optional[velocities], Optional[effective_masses])
+
+            The velocities and effective masses are given as the 1x3 trace and
+            full 3x3 tensor, respectively (along cartesian directions).
         """
         pass
+
+    def get_dos(self, kpoint_mesh: List[int], estep: float = 0.001,
+                width: float = 0.2, scissor: float = 0.0
+                ) -> Tuple[np.ndarray, np.ndarray, int]:
+        """Calculates the density of states using the interpolated bands.
+
+        Args:
+            kpoint_mesh: The k-point mesh as a 1x3 array. E.g.,``[6, 6, 6]``.
+            estep: The energy step, where smaller numbers give more
+                accuracy but are more expensive.
+            width: The gaussian smearing width.
+            scissor: The amount by which the band gap is scissored.
+
+        Returns:
+            The density of states data, formatted as::
+
+                (energies, densities, num_bands_considered)
+
+        """
+        mesh_data = np.array(self._sga.get_ir_reciprocal_mesh(kpoint_mesh))
+        ir_kpts = mesh_data[:, 0]
+        weights = mesh_data[:, 1] / mesh_data[:, 1].sum()
+
+        # (bands, nkpoints)
+        energies = self.get_energies(ir_kpts, scissor=scissor)
+
+        e_min = np.min(energies)
+        e_max = np.max(energies)
+
+        height = 1.0 / (width * np.sqrt(2 * np.pi))
+        e_points = int(round((e_max - e_min) / estep))
+        e_mesh = np.linspace(e_min, e_max, num=e_points, endpoint=True)
+        dos = np.zeros(len(e_mesh))
+
+        for ik, w in enumerate(weights):
+            for b in range(energies.shape[0]):
+                g = height * np.exp(
+                    -((e_mesh - energies[b, ik]) / width) ** 2 / 2.)
+                dos += w * g
+        return e_mesh, dos, energies.shape[0]
