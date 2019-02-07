a954ad79e194c91a41f5888284e187bf27315484
Alex Ganose <alexganose@googlemail.com>
2019-02-07 13:49:44 -0800

    Add normalization option to interpolation dos

--- amset/interpolate/base.py
+++ amset/interpolate/base.py
@@ -1,10 +1,14 @@
+import logging
+
 import numpy as np
 
 from abc import ABC, abstractmethod
 from logging import Logger
-from typing import Optional, Union, Tuple, List
+from typing import Optional, Union, Tuple, List, Sequence, Any
+from scipy.integrate import trapz
 
 from monty.json import MSONable
+
 from amset.logging import LoggableMixin
 from pymatgen.electronic_structure.bandstructure import BandStructure
 from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
@@ -14,12 +18,15 @@ class AbstractInterpolater(MSONable, LoggableMixin, ABC):
 
     def __init__(self, band_structure: BandStructure, num_electrons: int,
                  calc_dir: str = '.', logger: Union[bool, Logger] = True,
-                 log_level: Optional[int] = None, symprec: float = 0.01):
+                 log_level: int = logging.INFO, symprec: float = 0.01,
+                 soc: bool = False):
         self._band_structure = band_structure
         self._num_electrons = num_electrons
         self._calc_dir = calc_dir
-        self._logger = self.get_logger(logger, level=log_level)
+        self._logger = self.get_logger(logger)
         self._symprec = symprec
+        self._soc = soc
+        self._lattice_matrix = band_structure.structure.lattice.matrix
         self._sga = SpacegroupAnalyzer(band_structure.structure,
                                        symprec=symprec)
 
@@ -27,30 +34,6 @@ class AbstractInterpolater(MSONable, LoggableMixin, ABC):
     def initialize(self):
         """Initialise the interpolater."""
         pass
-
-    @abstractmethod
-    def get_energy(self, kpoint: np.ndarray, iband: int,
-                   return_velocity: bool = False,
-                   return_effective_mass: bool = False
-                   ) -> Union[float, Tuple[Union[float, np.ndarray], ...]]:
-        """Gets the interpolated energy for a specific k-point and band.
-
-        Args:
-            kpoint: The k-point fractional coordinates.
-            iband: The band index (0-indexed).
-            return_velocity: Whether to return the band velocity.
-            return_effective_mass: Whether to return the band effective mass.
-
-        Returns:
-            The band energies as a numpy array. If ``return_velocity`` or
-            ``return_effective_mass`` are ``True`` a tuple is returned,
-            formatted as::
-
-               (energy, Optional[velocity], Optional[effecitve_mass])
-
-            The velocity and effective mass are given as the 1x3 trace and
-            full 3x3 tensor, respectively (along cartesian directions).
-        """
 
     @abstractmethod
     def get_energies(self, kpoints: np.ndarray,
@@ -85,7 +68,7 @@ class AbstractInterpolater(MSONable, LoggableMixin, ABC):
     def get_dos(self, kpoint_mesh: List[int], emin: float = None,
                 emax: float = None, estep: float = 0.001,
                 width: float = 0.2, scissor: float = 0.0,
-                normalize: bool = False) -> Tuple[np.ndarray, np.ndarray]:
+                normalize: bool = False) -> np.ndarray:
         """Calculates the density of states using the interpolated bands.
 
         Args:
@@ -107,7 +90,7 @@ class AbstractInterpolater(MSONable, LoggableMixin, ABC):
 
         """
         mesh_data = np.array(self._sga.get_ir_reciprocal_mesh(kpoint_mesh))
-        ir_kpts = mesh_data[:, 0]
+        ir_kpts = np.asarray(list(map(list, mesh_data[:, 0])))
         weights = mesh_data[:, 1] / mesh_data[:, 1].sum()
 
         energies = self.get_energies(ir_kpts, scissor=scissor)
@@ -128,7 +111,43 @@ class AbstractInterpolater(MSONable, LoggableMixin, ABC):
                 dos += w * g
 
         if normalize:
+            # taken from old amset. Might reintroduce if needed to pass tests
+            # for idx in range(self.cbm_dos_idx, self.get_Eidx_in_dos(
+            #         self.cbm_vbm["n"]["energy"] + 2.0)):
+            #     dos[idx] = max(dos[idx], free_e_dos(
+            #         energy=self.dos_emesh[idx] - self.cbm_vbm["n"]["energy"]))
+            # for idx in range(
+            #         self.get_Eidx_in_dos(self.cbm_vbm["p"]["energy"] - 2.0),
+            #         self.vbm_dos_idx):
+            #     dos[idx] = max(dos[idx], free_e_dos(
+            #         energy=self.cbm_vbm["p"]["energy"] - self.dos_emesh[idx]))
+
             normalization_factor = nbands * (1 if self._soc else 2)
+            integ = trapz(dos, x=emesh)
+            self.logger.info("dos integral from {:.3f} to {:.3f} "
+                             "eV: {:.3f}".format(emin, emax, integ))
+            dos /= integ * normalization_factor
 
+        return np.column_stack((emesh, dos))
 
-        return emesh, dos
+    @staticmethod
+    def _simplify_return_data(data: Sequence[Any]) -> Union[Any, Sequence[Any]]:
+        """Helper function to prepare data to be returned.
+
+        Takes a collection of objects and:
+
+        - If the collection contains only a single item, return just that.
+        - Otherwise return the full collection.
+
+        Args:
+            data: The data.
+
+        Returns:
+            The first element of the collection if it only has one element, else
+            the full collection.
+        """
+
+        if len(data) == 1:
+            return data[0]
+        else:
+            return tuple(data)
