8192190bb591b6f4868abcbabc70f799fe8c978f
Alex Ganose <alexganose@googlemail.com>
2019-02-08 10:03:07 -0800

    Add interpolater init

---
+++ amset/interpolate/__init__.py
@@ -0,0 +1,3 @@
+from amset.interpolate.boltztrap1 import BoltzTraP1Interpolater
+from amset.interpolate.boltztrap2 import BoltzTraP2Interpolater
+from amset.interpolate.parabolic import ParabolicInterpolater
