0aea693bde8e82adbcfade84b65862ea93434a90
Alex Ganose <alexganose@googlemail.com>
2019-02-08 19:21:21 -0800

    Remove old tests
    Former-commit-id: af32e9219a02f1753414f2a0545a120277fa4b85 [formerly 7f0f44172cc28339e635e3e0f1cf364e2cb9efda] [formerly 3c75ac6b4bc6d2a6c79ba7fde3c2ff71bc3acfb9 [formerly b1520a5dfe244ce02784139adc04594bbb2544ed]] [formerly 544d19121a69b0e7c79c525d095f54e7ea479d2d [formerly 75a46250570c7eb305fa218951cd6d535928a98f] [formerly c641cb0c7cab62683d58ca263bdb2e840cd0ff05 [formerly 768b8207dd0041a81aec2b6d8f21dbadb8b236ad]]]
    Former-commit-id: 9e840d68d804f1d63f17a0ba323b0b94119e5fa7 [formerly bf5b3bbcd3313498c46c48668ec8f47454a3fb12] [formerly f6dd4b1598921359f276947f814aeab5f9aba24a [formerly ac7fbf2c57285b40a8a4d6f554ff7a97edc4ec29]]
    Former-commit-id: 1d37d99e134ae6c1cee6996575fd0457771e1b3a [formerly 6aa100448ec4777a391f75daabc510f7fffc611d]
    Former-commit-id: 3b421fb94db215f8658a90d1c34729e6cd07ad49

--- amset/tests/test_utils.py
+++ amset/tests/test_utils.py
@@ -7,7 +7,6 @@ import unittest
 from amset.utils.constants import comp_to_dirname
 from amset.utils.band_structure import kpts_to_first_bz, get_closest_k, \
     remove_duplicate_kpoints
-from amset.utils.band_interpolation import get_bs_extrema
 from pymatgen.io.vasp.outputs import Vasprun
 
 tdir = os.path.join(os.path.dirname(__file__), '..', '..', 'test_files')
@@ -20,69 +19,6 @@ warnings.simplefilter("ignore")
 
 
 class AmsetToolsTest(unittest.TestCase):
-
-    def listalmostequal(self, list1, list2, places=3):
-        for l1, l2 in zip(list1, list2):
-            self.assertAlmostEqual(l1, l2, places=places)
-
-    def test_get_bs_extrema(self):
-        extrema = get_bs_extrema(bs=vruns['GaAs'].get_band_structure(),
-                                 coeff_file=coeff_files['GaAs'], Ecut=1.0)
-        # first conduction band extrema:
-        self.listalmostequal(extrema['n'][0], [.0, .0, .0], 10)
-        self.listalmostequal(extrema['n'][1], [.0, -0.4701101, .0], 4)
-        self.assertEqual(len(extrema['n']), 2)
-        # last valence band extrema
-        self.listalmostequal(extrema['p'][0], [.0, .0, .0], 10)
-        self.listalmostequal(extrema['p'][1], [-0.31693, -0.04371, 0.], 4)
-        self.assertEqual(len(extrema['p']), 2)
-
-        si_extrema = get_bs_extrema(bs=vruns['Si'].get_band_structure(),
-                                    coeff_file=coeff_files['Si'], Ecut=1.0)
-        self.listalmostequal(si_extrema['n'][0], [0.419204, 0.419204, 0.], 4)
-        self.listalmostequal(si_extrema['n'][1], [-0.4638, -0.4638, -0.4638], 4)
-        self.listalmostequal(si_extrema['p'][0], [0.0, 0.0, 0.0], 10)
-        self.listalmostequal(si_extrema['p'][1], [-0.226681, -0.049923, 0.], 3)
-
-        # test can take a very long time. Commenting this out for now to
-        # avoid issues
-        # pbte_extrema = get_bs_extrema(bs=vruns['PbTe'].get_band_structure(),
-        #                               coeff_file=coeff_files['PbTe'], Ecut=1.0)
-        # self.listalmostequal(pbte_extrema['n'][0], [0., 0.5, 0.], 10)
-        # self.listalmostequal(pbte_extrema['n'][1], [.1522, -.0431, .1522], 4)
-        # self.listalmostequal(pbte_extrema['p'][0], [0., 0.5, 0.], 10)
-        # self.listalmostequal(pbte_extrema['p'][1], [.4784, -.2709, .2278], 3)
-        # self.listalmostequal(pbte_extrema['p'][2], [.162054, .162054, 0.], 3)
-        #
-        # inp_extrema = get_bs_extrema(bs=vruns['InP'].get_band_structure(),
-        #                              coeff_file=coeff_files['InP'], Ecut=1.0)
-        # self.listalmostequal(inp_extrema['n'][0], [0., 0.0, 0.], 10)
-        # self.listalmostequal(inp_extrema['n'][1], [0., 0.5, 0.], 10)
-        # self.listalmostequal(inp_extrema['p'][0], [0., 0.0, 0.], 10)
-        # self.listalmostequal(inp_extrema['p'][1], [-0.3843, -0.0325, 0.], 4)
-        #
-        # alcus2_extrema = get_bs_extrema(bs=vruns['AlCuS2'].get_band_structure(),
-        #                                 coeff_file=coeff_files['AlCuS2'],
-        #                                 Ecut=1.0)
-        # self.listalmostequal(alcus2_extrema['n'][0], [0., 0.0, 0.0], 10)
-        # self.listalmostequal(alcus2_extrema['n'][1], [0., 0.0, 0.5], 10)
-        # self.listalmostequal(alcus2_extrema['n'][2], [-0.49973, -0.49973, 0.],
-        #                      4)
-        # self.listalmostequal(alcus2_extrema['n'][3],
-        #                      [0.49047, 0.49047, 0.49818], 4)
-        # self.listalmostequal(alcus2_extrema['p'][0], [0., 0.0, 0.0], 10)
-        # self.listalmostequal(alcus2_extrema['p'][1], [0.28291, 0., -0.40218], 4)
-        # self.listalmostequal(alcus2_extrema['p'][2], [-0.25765, 0.25148, 0.], 4)
-        # self.listalmostequal(alcus2_extrema['p'][3], [-0.49973, -0.49973, 0.],
-        #                      4)
-        #
-        # in2o3_extrema = get_bs_extrema(bs=vruns['In2O3'].get_band_structure(),
-        #                                coeff_file=coeff_files['In2O3'],
-        #                                Ecut=1.0)
-        # self.listalmostequal(in2o3_extrema['n'][0], [0., 0.0, 0.0], 10)
-        # self.listalmostequal(in2o3_extrema['p'][0], [0., 0.09631, 0.0], 4)
-        # self.listalmostequal(in2o3_extrema['p'][1], [0.30498, 0.30498, 0.18299],
-        #                      4)
 
     def test_kpts_to_first_BZ(self):
         kpts_orig = [[0.51, 1.00, -0.50], [1.40, -1.20, 0.49]]
