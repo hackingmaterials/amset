2ee6cc412da279a6b03a8f237ac930526c4f988a
Alex Ganose <alexganose@googlemail.com>
2019-02-11 14:25:53 -0800

    Update get extrema tests
    Former-commit-id: d644c0ec586566e3e941dadab081c251d215fc88 [formerly 4af9fc370bf82c67ca38fbac5067863006940aca] [formerly 6bb629055c3ec660b7ad1b6b1544d7a16a58ffb7 [formerly 566b6457ea8777d29aeff61a7f23ee008382778d]] [formerly 542efd42d4e1febd05733e0fbfe516ef9cdf0cdc [formerly 4b0705299533b3950d32844a971173b021bf5980] [formerly 2d3f0a477fa14b4b80d7067beee82d4d668b6571 [formerly ff91b0ace340917e43ea75ca94b5cdea05c454c6]]]
    Former-commit-id: f6b7f9973b549836e09a7ebabacc8474bb552749 [formerly 3a97f0418b4ab26eac337b8d5ad27c37dc3b144f] [formerly f2bbd3f912d463e2a69a23fd0ae3d48d306c5528 [formerly 2750ec313cc94be179e07e255f668a0d75eb60a5]]
    Former-commit-id: 1785c2776c07522ba20650fb6c0d3a03836147ff [formerly 7196132b2549e03ab31347e60180816dd79f521f]
    Former-commit-id: a3a0fb6923d1801964443a67f6e2ae89d41133f7

--- amset/interpolate/tests/test_parabolic.py
+++ amset/interpolate/tests/test_parabolic.py
@@ -18,8 +18,9 @@ class TestParabolicInterpolater(unittest.TestCase):
         vr = Vasprun(os.path.join(amset_files, 'vasprun.xml'))
         bs = vr.get_band_structure()
         num_electrons = vr.parameters['NELECT']
-        band_parameters = [[[[0.5, 0.5, 0.5], 0, 0.1]],
-                           [[[0, 0, 0], 0.5, 0.2]]]
+        band_parameters = [[[[0.5, 0.5, 0.5], 0, 0.1],
+                            [[0, 0, 0], 0.5, 0.2]],
+                           [[[0, 0, 0], 1, 0.1]]]
 
         self.kpoints = np.array(vr.actual_kpoints)
         self.interpolater = ParabolicInterpolater(bs, num_electrons,
@@ -38,36 +39,36 @@ class TestParabolicInterpolater(unittest.TestCase):
             self.kpoints, 31, return_velocity=False,
             return_effective_mass=False)
         self.assertEqual(energies.shape, (138,))
-        self.assertAlmostEqual(energies[0], 4.480020375037921)
+        self.assertAlmostEqual(energies[0], 4.742)
 
         # test energy + velocity
         energies, velocities = self.interpolater.get_energies(
             self.kpoints, 31, return_velocity=True,
             return_effective_mass=False)
         self.assertEqual(energies.shape, (138,))
-        self.assertAlmostEqual(energies[0], 4.480020375037921)
+        self.assertAlmostEqual(energies[0], 4.742)
         self.assertEqual(velocities.shape, (138, 3))
-        self.assertAlmostEqual(velocities[10][0], 117167212.04476692)
+        self.assertAlmostEqual(velocities[10][0], 292836222.3145536)
 
         # test energy + effective_mass
         energies, effective_masses = self.interpolater.get_energies(
             self.kpoints, 31, return_velocity=False,
             return_effective_mass=True)
         self.assertEqual(energies.shape, (138,))
-        self.assertAlmostEqual(energies[0], 4.480020375037921)
+        self.assertAlmostEqual(energies[0], 4.742)
         self.assertEqual(effective_masses.shape, (138, 3, 3))
-        self.assertAlmostEqual(effective_masses[10][0][0], -0.1)
+        self.assertAlmostEqual(effective_masses[10][0][0], -0.2)
 
         # test energy + velocity + effective_mass
         energies, velocities, effective_masses = self.interpolater.get_energies(
             self.kpoints, 31, return_velocity=True,
             return_effective_mass=True)
         self.assertEqual(energies.shape, (138,))
-        self.assertAlmostEqual(energies[0], 4.480020375037921)
+        self.assertAlmostEqual(energies[0], 4.742)
         self.assertEqual(velocities.shape, (138, 3))
-        self.assertAlmostEqual(velocities[10][0], 117167212.04476692)
+        self.assertAlmostEqual(velocities[10][0], 292836222.3145536)
         self.assertEqual(effective_masses.shape, (138, 3, 3))
-        self.assertAlmostEqual(effective_masses[10][0][0], -0.1)
+        self.assertAlmostEqual(effective_masses[10][0][0], -0.2)
 
     def test_get_energies_scissor(self):
         """Test scissoring of band energies."""
@@ -77,11 +78,11 @@ class TestParabolicInterpolater(unittest.TestCase):
             self.kpoints, 31, return_velocity=False,
             return_effective_mass=False, scissor=1.)
         self.assertEqual(energies.shape, (138,))
-        self.assertAlmostEqual(energies[0], 4.480020375037921 - 0.5)
+        self.assertAlmostEqual(energies[0], 4.742 - 0.5)
 
         # test conduction band
         energies = self.interpolater.get_energies(
-            self.kpoints, 33, return_velocity=False,
+            self.kpoints, 32, return_velocity=False,
             return_effective_mass=False, scissor=1.)
         self.assertEqual(energies.shape, (138,))
         self.assertAlmostEqual(energies[0], 7.5015 + 0.5)
@@ -91,46 +92,46 @@ class TestParabolicInterpolater(unittest.TestCase):
 
         # test just getting energy
         energies = self.interpolater.get_energies(
-            self.kpoints, [25, 35], return_velocity=False,
+            self.kpoints, [31, 32], return_velocity=False,
             return_effective_mass=False)
         self.assertEqual(energies.shape, (2, 138,))
-        self.assertAlmostEqual(energies[0][0], 4.480020375037921)
+        self.assertAlmostEqual(energies[0][0], 4.742)
         self.assertAlmostEqual(energies[1][0], 7.5015)
 
         # test energy + velocity
         energies, velocities = self.interpolater.get_energies(
-            self.kpoints, [25, 35], return_velocity=True,
+            self.kpoints, [31, 32], return_velocity=True,
             return_effective_mass=False)
         self.assertEqual(energies.shape, (2, 138,))
-        self.assertAlmostEqual(energies[0][0], 4.480020375037921)
+        self.assertAlmostEqual(energies[0][0], 4.742)
         self.assertAlmostEqual(energies[1][0], 7.5015)
         self.assertEqual(velocities.shape, (2, 138, 3))
-        self.assertAlmostEqual(velocities[0][10][0], 117167212.04476692)
-        self.assertAlmostEqual(velocities[1][10][0], 29283622.231455356)
+        self.assertAlmostEqual(velocities[0][10][0], 292836222.3145536)
+        self.assertAlmostEqual(velocities[1][10][0], 292836222.3145536)
 
         # test energy + effective_mass
         energies, effective_masses = self.interpolater.get_energies(
-            self.kpoints, [25, 35], return_velocity=False,
+            self.kpoints, [31, 32], return_velocity=False,
             return_effective_mass=True)
         self.assertEqual(energies.shape, (2, 138,))
-        self.assertAlmostEqual(energies[0][0], 4.480020375037921)
+        self.assertAlmostEqual(energies[0][0], 4.742)
         self.assertAlmostEqual(energies[1][0], 7.5015)
         self.assertEqual(effective_masses.shape, (2, 138, 3, 3))
-        self.assertAlmostEqual(effective_masses[0][10][0][0], -0.1)
+        self.assertAlmostEqual(effective_masses[0][10][0][0], -0.2)
         self.assertAlmostEqual(effective_masses[1][10][0][0], 0.2)
 
         # test energy + velocity + effective_mass
         energies, velocities, effective_masses = self.interpolater.get_energies(
-            self.kpoints, [31, 33], return_velocity=True,
+            self.kpoints, [31, 32], return_velocity=True,
             return_effective_mass=True)
         self.assertEqual(energies.shape, (2, 138,))
-        self.assertAlmostEqual(energies[0][0], 4.480020375037921)
+        self.assertAlmostEqual(energies[0][0], 4.742)
         self.assertAlmostEqual(energies[1][0], 7.5015)
         self.assertEqual(velocities.shape, (2, 138, 3))
-        self.assertAlmostEqual(velocities[0][10][0], 117167212.04476692)
-        self.assertAlmostEqual(velocities[1][10][0], 29283622.231455356)
+        self.assertAlmostEqual(velocities[0][10][0], 292836222.3145536)
+        self.assertAlmostEqual(velocities[1][10][0], 292836222.3145536)
         self.assertEqual(effective_masses.shape, (2, 138, 3, 3))
-        self.assertAlmostEqual(effective_masses[0][10][0][0], -0.1)
+        self.assertAlmostEqual(effective_masses[0][10][0][0], -0.2)
         self.assertAlmostEqual(effective_masses[1][10][0][0], 0.2)
 
     def test_get_energies_all_bands(self):
@@ -139,14 +140,14 @@ class TestParabolicInterpolater(unittest.TestCase):
             self.kpoints, None, return_velocity=True,
             return_effective_mass=True)
         self.assertEqual(energies.shape, (4, 138,))
-        self.assertAlmostEqual(energies[1][0], 4.480020375037921)
-        self.assertAlmostEqual(energies[3][0], 7.5015)
+        self.assertAlmostEqual(energies[0][0], 4.242)
+        self.assertAlmostEqual(energies[1][0], 4.742)
         self.assertEqual(velocities.shape, (4, 138, 3))
-        self.assertAlmostEqual(velocities[1][10][0], 117167212.04476692)
-        self.assertAlmostEqual(velocities[3][10][0], 29283622.231455356)
+        self.assertAlmostEqual(velocities[0][10][0], 585672444.6291072)
+        self.assertAlmostEqual(velocities[1][10][0], 292836222.3145536)
         self.assertEqual(effective_masses.shape, (4, 138, 3, 3))
-        self.assertAlmostEqual(effective_masses[1][10][0][0], -0.1)
-        self.assertAlmostEqual(effective_masses[3][10][0][0], 0.2)
+        self.assertAlmostEqual(effective_masses[0][10][0][0], -0.1)
+        self.assertAlmostEqual(effective_masses[1][10][0][0], -0.2)
 
     def test_get_dos(self):
         """Test generating the interpolated DOS."""
@@ -154,34 +155,26 @@ class TestParabolicInterpolater(unittest.TestCase):
                                         width=0.075)
         self.assertEqual(dos.shape, (20000, 2))
         self.assertEqual(dos[0][0], -10)
-        self.assertAlmostEqual(dos[15000][1], 1.9513959920355906)
+        self.assertAlmostEqual(dos[10000][1], 0.04839047875200659)
 
         # test normalising the DOS
         dos = self.interpolater.get_dos([10, 10, 10], emin=-10, emax=10,
                                         width=0.075, normalize=True)
         self.assertEqual(dos.shape, (20000, 2))
         self.assertEqual(dos[0][0], -10)
-        self.assertAlmostEqual(dos[15000][1], 0.060981124751112205)
+        self.assertAlmostEqual(dos[10000][1], 0.0037456102434132218)
 
-    # def test_get_extrema(self):
-    #     """Test getting the band structure extrema."""
-    #
-    #     # test VBM
-    #     extrema = self.interpolater.get_extrema(31, e_cut=1.)
-    #     print(extrema)
-    #     np.testing.assert_array_almost_equal(extrema[0], [0., 0.0, 0.0], 10)
-    #     np.testing.assert_array_almost_equal(extrema[1],
-    #                                          [0.28291, 0., -0.40218], 4)
-    #     np.testing.assert_array_almost_equal(extrema[2],
-    #                                          [-0.25765, 0.25148, 0.], 4)
-    #     np.testing.assert_array_almost_equal(extrema[3],
-    #                                          [-0.49973, -0.49973, 0.], 4)
-    #
-    #     # test CBM
-    #     extrema = self.interpolater.get_extrema(32, e_cut=1.)
-    #     np.testing.assert_array_almost_equal(extrema[0], [0., 0.0, 0.0], 10)
-    #     np.testing.assert_array_almost_equal(extrema[1], [0., 0.0, 0.5], 10)
-    #     np.testing.assert_array_almost_equal(extrema[2],
-    #                                          [-0.49973, -0.49973, 0.], 4)
-    #     np.testing.assert_array_almost_equal(extrema[3],
-    #                                          [0.49047, 0.49047, 0.49818], 4)
+    def test_get_extrema(self):
+        """Test getting the band structure extrema."""
+
+        # test VBM
+        extrema = self.interpolater.get_extrema(31, e_cut=1)
+        self.assertEqual(len(extrema), 2)
+        np.testing.assert_array_almost_equal(extrema[0], [0.5, -0.5, -1], 10)
+        np.testing.assert_array_almost_equal(extrema[1], [0., 0., 0.], 10)
+
+        # test CBM
+        extrema = self.interpolater.get_extrema(32, e_cut=1.)
+        self.assertEqual(len(extrema), 2)
+        np.testing.assert_array_almost_equal(extrema[0], [0.5, -0.5, -1], 10)
+        np.testing.assert_array_almost_equal(extrema[1], [0., 0., 0.], 10)
