88af27d8f463d87cb08b0a2bec5c1d8c0da2e564
Alex Ganose <alexganose@googlemail.com>
2019-05-24 10:59:55 -0700

    Add basic Si example

---
+++ examples/Si.py
@@ -0,0 +1,34 @@
+import warnings
+
+from amset.util import initialize_amset_logger
+from amset.run import AmsetRunner
+
+settings = {
+    "general": {
+        "user_bandgap": None,
+        "interpolation_factor": 3,
+        "scattering_type": "auto",
+        "doping": [1e15],
+        "temperatures": [200, 300]
+    },
+    "material": {
+        "deformation_potential": 6.5,
+        "elastic_constant": 190,
+    },
+    "performance": {
+        "energy_tol": 0.001,
+        "energy_cutoff": 1.5,
+        "g_tol": 1,
+        "symprec": None,
+        "nworkers": -1,
+        "log_traceback": True
+
+    }
+}
+
+warnings.simplefilter("ignore")
+
+initialize_amset_logger(log_traceback=settings["output"]["log_traceback"])
+
+runner = AmsetRunner.from_vasprun_and_settings("vasprun.xml", settings)
+runner.run()
