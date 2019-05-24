1e2d8f43bc45ba07975b99142d6eb665699a5615
Alex Ganose <alexganose@googlemail.com>
2019-05-23 18:43:17 -0700

    WIP: Improvements to runner class
    Former-commit-id: 5a27edb701445b187c12722528e3958dc844b769 [formerly be86f904a745fa7d4256bfef9e3e09b310f537f4] [formerly d49ac0ba4d2a1a04edcd8d9a5fb765031076642c [formerly 1d88d499e85f7337d9548a1a3dd8bafa94ea8706]] [formerly 70f92a67c2abe21873e4b71d88863a8a9e3dac2e [formerly 453e6212c0e0d3890ae5b15a717345962d11823d] [formerly 684b096048bee2c428d5b32d3bc3ce20dc548a99 [formerly 2203c5f4edf16665d6162849fb096ff881d6cee8]]]
    Former-commit-id: 8a69ea94ff25f7c536b41ed2eb92a9017bf25c5f [formerly bd283d16dd7d136eedf8382cb8d58aeeb9403542] [formerly faac1e16828912c92865ee103962079f285d4537 [formerly ec3875718697498f2da6db3a6affed9917e9e133]]
    Former-commit-id: 8ea05a0261cb16c7bb5ddb2f139334479c07ddcb [formerly 0946ca1248f3b628ac3915c270426ff2249f6305]
    Former-commit-id: f4bab5cf76be7a62f9d188e357aab9ee269f3678

--- amset/run.py
+++ amset/run.py
@@ -1,3 +1,4 @@
+import os
 import copy
 import datetime
 import logging
@@ -5,12 +6,16 @@ import logging
 import numpy as np
 
 from pathlib import Path
+from os.path import join as joinpath
 from typing import Optional, Any, Dict, Union, List
 
 from monty.json import MSONable
 
 from amset.interpolate import Interpolater
+from amset.io import load_settings_from_file
 from amset.scatter import ScatteringCalculator
+from amset.solve import BTESolver
+from amset.util import validate_settings, star_log
 from pymatgen.electronic_structure.bandstructure import BandStructure
 from pymatgen.io.vasp import Vasprun
 from amset import __version__, amset_defaults
@@ -26,7 +31,7 @@ class AmsetRunner(MSONable):
                  material_properties: Dict[str, Any],
                  doping: Optional[Union[List, np.ndarray]] = None,
                  temperatures: Optional[Union[List, np.ndarray]] = None,
-                 scattering: Optional[Union[str, List[str], float]] = "auto",
+                 scattering_type: Optional[Union[str, List[str], float]] = "auto",
                  performance_parameters: Optional[Dict[str, float]] = None,
                  interpolation_factor: int = 10,
                  scissor: Optional[float] = None,
@@ -34,7 +39,7 @@ class AmsetRunner(MSONable):
                  soc: bool = False):
         self._band_structure = band_structure
         self._num_electrons = num_electrons
-        self.scattering = scattering
+        self.scattering_type = scattering_type
         self.interpolation_factor = interpolation_factor
         self._scissor = scissor
         self._user_bandgap = user_bandgap
@@ -53,7 +58,7 @@ class AmsetRunner(MSONable):
         # if the user doesn't specify a value then use the default
         params = copy.deepcopy(amset_defaults)
         self.performance_parameters = params["performance"]
-        self.material_properties = params["materials"]
+        self.material_properties = params["material"]
         self.performance_parameters.update(performance_parameters)
         self.material_properties.update(material_properties)
 
@@ -67,14 +72,16 @@ class AmsetRunner(MSONable):
         # _log_settings(self)
 
         # initialize scattering first so we can check that materials properties
-        # and desired scattering rates are consistent
+        # and desired scattering mechanisms are consistent
         scatter = ScatteringCalculator(
             self.material_properties, self.doping, self.temperatures,
-            scattering=self.scattering,
+            scattering_type=self.scattering_type,
             energy_tol=self.performance_parameters["energy_tol"],
             g_tol=self.performance_parameters["g_tol"],
             use_symmetry=self.performance_parameters["symprec"] is not None,
             nworkers=self.performance_parameters["nworkers"])
+
+        star_log(logger, "INTERPOLATION")
 
         interpolater = Interpolater(
             self._band_structure, num_electrons=self._num_electrons,
@@ -82,7 +89,7 @@ class AmsetRunner(MSONable):
             interpolate_projections=True)
 
         electronic_structure = interpolater.get_electronic_structure(
-            energy_cutoff=self.performance_parameters,
+            energy_cutoff=self.performance_parameters["energy_cutoff"],
             scissor=self._scissor, bandgap=self._user_bandgap,
             dos_estep=self.performance_parameters["dos_estep"],
             dos_width=self.performance_parameters["dos_width"],
@@ -92,10 +99,17 @@ class AmsetRunner(MSONable):
         electronic_structure.set_doping_and_temperatures(
             self.doping, self.temperatures)
 
-        electronic_structure.set_scattering_rates(
-            scatter.calculate_scattering_rates(electronic_structure))
+        star_log(logger, "SCATTERING")
 
-        # solver = BoltzTraPSolver()
+        electronic_structure.set_scattering_rates(
+            scatter.calculate_scattering_rates(electronic_structure),
+            [m.name for m in scatter.scatterers])
+
+        # TODO: make these options configurable
+        solver = BTESolver(calculate_mobility=True,
+                           separate_scattering_mobilities=True)
+        sigma, seebeck, kappa, hall, mobility = solver.solve_bte(
+            electronic_structure)
 
     @staticmethod
     def from_vasprun(vasprun: Union[str, Path, Vasprun],
@@ -125,25 +139,59 @@ class AmsetRunner(MSONable):
         return AmsetRunner(band_structure, nelect, material_parameters,
                            soc=soc, **kwargs)
 
-    # from folder constructor
+    @staticmethod
+    def from_vasprun_and_settings(vasprun, settings):
+        settings = validate_settings(settings)
+
+        return AmsetRunner.from_vasprun(
+            vasprun, settings["material"],
+            doping=settings["general"]["doping"],
+            temperatures=settings["general"]["temperatures"],
+            scattering_type=settings["general"]["scattering_type"],
+            performance_parameters=settings["performance"],
+            interpolation_factor=settings["general"]["interpolation_factor"],
+            scissor=settings["general"]["scissor"],
+            user_bandgap=settings["general"]["bandgap"])
+
+    @staticmethod
+    def from_directory(directory: Union[str, Path] = '.',
+                       vasprun: Optional[Union[str, Path]] = None,
+                       settings_file: Optional[Union[str, Path]] = None):
+        if not vasprun:
+            vr_file = joinpath(directory, 'vasprun.xml')
+            vr_file_gz = joinpath(directory, 'vasprun.xml.gz')
+
+            if os.path.exists(vr_file):
+                vasprun = Vasprun(vr_file)
+            elif os.path.exists(vr_file_gz):
+                vasprun = Vasprun(vr_file_gz)
+            else:
+                msg = 'No vasprun.xml found in {}'.format(directory)
+                logger.error(msg)
+                raise FileNotFoundError(msg)
+
+        if not settings_file:
+            settings_file = joinpath(directory, "settings.yaml")
+        settings = load_settings_from_file(settings_file)
+
+        return AmsetRunner.from_vasprun_and_settings(vasprun, settings)
 
 
 def _log_amset_intro():
     now = datetime.datetime.now()
     logger.info("""
-                █████╗ ███╗   ███╗███████╗███████╗████████╗
-               ██╔══██╗████╗ ████║██╔════╝██╔════╝╚══██╔══╝
-               ███████║██╔████╔██║███████╗█████╗     ██║   
-               ██╔══██║██║╚██╔╝██║╚════██║██╔══╝     ██║   
-               ██║  ██║██║ ╚═╝ ██║███████║███████╗   ██║   
-               ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝╚══════╝   ╚═╝   
-               
-                                             v{}
+               █████╗ ███╗   ███╗███████╗███████╗████████╗
+              ██╔══██╗████╗ ████║██╔════╝██╔════╝╚══██╔══╝
+              ███████║██╔████╔██║███████╗█████╗     ██║   
+              ██╔══██║██║╚██╔╝██║╚════██║██╔══╝     ██║   
+              ██║  ██║██║ ╚═╝ ██║███████║███████╗   ██║   
+              ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝╚══════╝   ╚═╝   
+              
+                                                  v{}
                                              
     A. Ganose, A. Faghaninia, J. Park, F. Ricci, R. Woods-Robinson, 
     J. Frost,  K. Persson, G. Hautier, A. Jain, in prep.
     
     
-    amset starting on {} at {}""".format(
+  amset starting on {} at {}""".format(
         __version__, now.strftime("%d %b %Y"), now.strftime("%H:%M")))
-
