a23fbbd3af606202f46417a8feeae2bb0add6016
Alex Ganose <alexganose@googlemail.com>
2019-06-10 18:33:40 -0700

    Additional work on densify module
    Former-commit-id: 7ee97900fa933c043131c691c6609a1f4876fe0c [formerly 8a371dde6cd0f17fa0304c3f98254e0ac4983a5e] [formerly 91aacfeb84c7e8f84698512404fe09eefb3b625b [formerly 68f107492ab42116ca07e6f1991197d6a871d2a1]] [formerly 9cce2581c7749f52eac7ba6faac3f44611c7ebc6 [formerly 40a32134c0faa441b8d70d8632dd2a64397ab7fc] [formerly 500fcc3693bcddd53c3d78c3fcdc94f89c682044 [formerly d120833d24f9fad144b3079df173665a86f4a2c1]]]
    Former-commit-id: 050ceb59b84d37c12bfafcd5beb53cd500f8d2fd [formerly 051a425f43bf4c6a2f952a8cfae520873266b66d] [formerly dc3a53a3a2abd67dc15a1602244afc42da84b44d [formerly 620086a34a7bbe8c4f86c5f000d2bb2b89bfc0c9]]
    Former-commit-id: 0b57c3a4ab32a75fb421f5aa7a9e02894a5250c0 [formerly 33c00d40752c890582bc7bb9af5ad134206e3e3f]
    Former-commit-id: ad8932f7c61482bf9326134372182af72b1560ed

--- amset/voronoi.py
+++ amset/voronoi.py
@@ -1,18 +1,31 @@
-from typing import Optional
+import itertools
+import logging
+import sys
+from typing import Optional, Tuple, Union
 
 import numpy as np
+from scipy.spatial.qhull import Voronoi, ConvexHull
+from scipy.stats import binned_statistic_dd
+from tqdm import tqdm
+
 from amset import amset_defaults
+from amset.constants import output_width
+from amset.log import log_list
 
 pdefaults = amset_defaults["performance"]
+
+logger = logging.getLogger(__name__)
 
 
 class PeriodicVoronoi(object):
     """
-    Note this class works because most of the points are on a regular grid.
+    Note this class works because most of the points are on a regular grid so
+    it is valid to calculate the Voronoi diagram in blocks.
     """
 
     def __init__(self,
                  frac_points: np.ndarray,
+                 reciprocal_lattice_matrix: Optional[np.ndarray] = None,
                  original_mesh: Optional[np.ndarray] = None,
                  nworkers: int = pdefaults["nworkers"]):
         """
@@ -27,12 +40,16 @@ class PeriodicVoronoi(object):
         self._original_mesh = original_mesh
         self.frac_points = frac_points
 
-        if original_mesh:
-            grid_length_by_axis = 5 / original_mesh
-        else:
-            grid_length_by_axis = [0.05] * 3
+        if not reciprocal_lattice_matrix:
+            reciprocal_lattice_matrix = np.diag([1, 1, 1])
 
-        n_blocks_by_axis = np.ceil(1/grid_length_by_axis)
+        if original_mesh is None:
+            self._grid_length_by_axis = [0.05] * 3
+        else:
+            self._grid_length_by_axis = 2 / original_mesh
+
+        self._n_blocks_by_axis = np.ceil(
+            1 / self._grid_length_by_axis).astype(int)
 
         # In order to take into account periodic boundary conditions we repeat
         # the points a number of times in each direction. Note this method
@@ -42,41 +59,153 @@ class PeriodicVoronoi(object):
         dim = [-1, 0, 1]
         periodic_points = []
 
-        for image in np.itertools.product(dim, dim, dim):
+        for image in itertools.product(dim, dim, dim):
             if image[0] == image[1] == image[2] == 0:
                 # don't add the original points here
                 continue
             periodic_points.append(frac_points + image)
 
+        # add the original points at the end so we know their indices
+        periodic_points.append(frac_points)
+        self._periodic_points = np.concatenate(periodic_points)
+        self._periodic_points_cart = np.dot(
+            self._periodic_points, reciprocal_lattice_matrix)
+        self._periodic_idx = np.arange(len(self._periodic_points))
+
+        # frac_points_idx is the index of the original frac points in the
+        # periodic mesh
+        self._frac_points_idx = self._periodic_idx[-len(frac_points):]
+
         # limits is ((xmin, xmax), (ymin, ymax), (zmin, zmax))
-        limits = np.stack(((-0.5 - grid_length_by_axis),
-                           (0.5 + grid_length_by_axis)), axis=1)
+        limits = np.stack(((-0.5 - self._grid_length_by_axis),
+                           (0.5 + self._grid_length_by_axis)), axis=1)
 
         # group the points by their coordinates, with the group cutoffs defined
         # by multiples of inner_grid_length. Include two extra groups on either
         # side of the unit cell. I.e. if inner_grid_length is 0.2,
         # there will be 7 groups for each dimension, resulting in 343 total
         # groups, with the limits ranging from -0.7 to 0.7. The groups are
-        # indexed using nx, ny, nz, which range from 0 to
+        # indexed using nx, ny, nz, which range from 1 to
         # n_blocks_by_axis + 2, the bins containing the original point set
-        # range from 1 to n_blocks_by_axis - 1, however, these bins
+        # range from 2 to n_blocks_by_axis, however, these bins
         # may also contain additional points due to aliasing errors.
+        _, _, self._groups_idx = binned_statistic_dd(
+            self._periodic_points, np.ones(len(self._periodic_points)),
+            bins=self._n_blocks_by_axis + 2, range=limits,
+            expand_binnumbers=True)
 
-        # doesn't work...
-        self._groups_idx, _ = np.histogramdd(
-            periodic_points, n_blocks_by_axis + 2, range=limits)
+    def compute_volumes(self):
+        logger.info("Calculating k-point Voronoi volumes in blocks:")
+        logger.debug("  ├── # total k-points: {}".format(len(self.frac_points)))
+        logger.debug("  ├── # blocks: {:d}".format(np.product(
+            self._n_blocks_by_axis)))
 
-    def compute(self, return_volumes):
+        # b_idx is the list of buffer cells, with the form:
+        # [[1, x], [1, y], [1, z]] where x, y, and z are the buffer cells on the
+        # far side of the unitcell
+        b_idx = np.stack(([1, 1, 1], self._n_blocks_by_axis + 2), axis=1)
+
+        volumes = np.zeros(self.frac_points.shape[0])
+        pbar = tqdm(total=volumes.shape[0], ncols=output_width,
+                    desc="    ├── progress", file=sys.stdout,
+                    bar_format='{l_bar}{bar}| {elapsed}<{remaining}{postfix}')
+
+        for nx, ny, nz in np.ndindex(tuple(self._n_blocks_by_axis + 2)):
+            # group numbers start from 1
+            nx += 1
+            ny += 1
+            nz += 1
+
+            if nx in b_idx[0] or ny in b_idx[1] or nz in b_idx[2]:
+                # cell is in the buffer zone outside the unitcell therefore
+                # don't calculate voronoi weights
+                continue
+
+            # get the indices of the points to include when calculating the
+            # Voronoi diagram, this includes the block of interest and the
+            # blocks immediately surrounding it
+            voro_idx = self._get_idx_by_group(
+                (nx - 1, nx + 1), (ny - 1, ny + 1), (nz - 1, nz + 1))
+            print("n1 group", self._get_idx_by_group(5, 5, 5))
+
+            # get the indices of any unit cell points in voro_idx, I.e.
+            # it is the index of the point in voro_idx, not the index of the
+            # point in the original periodic mesh. Allows us to map from the
+            # voro results to the original periodic mesh
+            voro_to_block_idx = _get_loc(voro_idx, self._frac_points_idx)
+            print(voro_to_block_idx.shape)
+
+            # Now get the indices of the unit cell points (in the periodic mesh)
+            # that we are including in the voronoi diagram
+            points_in_voro = voro_idx[voro_to_block_idx]
+            # print(points_in_voro)
+
+            # finally, normalise these indices to get their corresponding index
+            # in the original frac_points mesh. As we put the original
+            # frac_points at the end of the periodic_points mesh this is easy.
+            points_in_voro -= len(self._periodic_points) - len(self.frac_points)
+
+            voro = Voronoi(self._periodic_points_cart[voro_idx])
+            volumes[points_in_voro] = _get_voronoi_volumes(
+                voro, voro_to_block_idx)
+
+            pbar.update(len(points_in_voro))
+
+        return volumes
+
+    def _get_idx_by_group(self,
+                          nx: Union[int, Tuple[int, int]],
+                          ny: Union[int, Tuple[int, int]],
+                          nz: Union[int, Tuple[int, int]]):
+        if isinstance(nx, tuple):
+            xmask = ((self._groups_idx[0] >= nx[0]) &
+                     (self._groups_idx[0] <= nx[1]))
+        else:
+            xmask = self._groups_idx[0] == nx
+
+        if isinstance(ny, tuple):
+            ymask = ((self._groups_idx[1] >= ny[0]) &
+                     (self._groups_idx[1] <= ny[1]))
+        else:
+            ymask = self._groups_idx[1] == ny
+
+        if isinstance(nz, tuple):
+            zmask = ((self._groups_idx[2] >= nz[0]) &
+                     (self._groups_idx[2] <= nz[1]))
+        else:
+            zmask = self._groups_idx[2] == nz
+
+        return self._periodic_idx[xmask & ymask & zmask]
 
 
+def _get_voronoi_volumes(voronoi, volume_indices) -> np.ndarray:
+    volumes = np.zeros(len(volume_indices))
+
+    for i, reg_num in enumerate(voronoi.point_region[volume_indices]):
+        indices = voronoi.regions[reg_num]
+        if -1 in indices:
+            # some regions can be opened
+            volumes[i] = np.inf
+        else:
+            volumes[i] = ConvexHull(voronoi.vertices[indices]).volume
+    return volumes
 
 
+def _get_loc(x, y):
+    """
+    Based on https://stackoverflow.com/questions/8251541
 
+    Args:
+        x:
+        y:
 
+    Returns:
 
-
-
-
-
-
-
+    """
+    # len(x) > len(y)
+    index = np.argsort(x)
+    sorted_x = x[index]
+    sorted_index = np.searchsorted(sorted_x, y)
+    yindex = np.take(index, sorted_index, mode="clip")
+    mask = x[yindex] == y
+    return yindex[mask]
