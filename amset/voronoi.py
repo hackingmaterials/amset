5793c4bbece50be92deb8dea29b01ae9d549ed67
Alex Ganose <alexganose@googlemail.com>
2019-06-11 13:06:17 -0700

    Add multiprocessing to Voronoi

--- amset/voronoi.py
+++ amset/voronoi.py
@@ -1,6 +1,10 @@
 import itertools
 import logging
 import sys
+from multiprocessing import cpu_count, Process, Queue
+
+import numexpr as ne
+
 from typing import Optional, Tuple, Union
 
 import numpy as np
@@ -10,7 +14,7 @@ from tqdm import tqdm
 
 from amset import amset_defaults
 from amset.constants import output_width
-from amset.log import log_list
+from amset.util import create_shared_array
 
 pdefaults = amset_defaults["performance"]
 
@@ -36,7 +40,7 @@ class PeriodicVoronoi(object):
             original_mesh:
             nworkers:
         """
-        self._nworkers = nworkers
+        self._nworkers = nworkers if nworkers != -1 else cpu_count()
         self._original_mesh = original_mesh
         self.frac_points = frac_points
 
@@ -70,11 +74,6 @@ class PeriodicVoronoi(object):
         self._periodic_points = np.concatenate(periodic_points)
         self._periodic_points_cart = np.dot(
             self._periodic_points, reciprocal_lattice_matrix)
-        self._periodic_idx = np.arange(len(self._periodic_points))
-
-        # frac_points_idx is the index of the original frac points in the
-        # periodic mesh
-        self._frac_points_idx = self._periodic_idx[-len(frac_points):]
 
         # limits is ((xmin, xmax), (ymin, ymax), (zmin, zmax))
         limits = np.stack(((-0.5 - self._grid_length_by_axis),
@@ -100,96 +99,166 @@ class PeriodicVoronoi(object):
         logger.debug("  ├── # blocks: {:d}".format(np.product(
             self._n_blocks_by_axis)))
 
-        # b_idx is the list of buffer cells, with the form:
+        # buffer is the list of buffer cells, with the form:
         # [[1, x], [1, y], [1, z]] where x, y, and z are the buffer cells on the
         # far side of the unitcell
-        b_idx = np.stack(([1, 1, 1], self._n_blocks_by_axis + 2), axis=1)
+        buffer = np.stack(([1, 1, 1], self._n_blocks_by_axis + 2), axis=1)
 
-        volumes = np.zeros(self.frac_points.shape[0])
-        pbar = tqdm(total=volumes.shape[0], ncols=output_width,
+        pbar = tqdm(total=len(self.frac_points), ncols=output_width,
                     desc="    ├── progress", file=sys.stdout,
                     bar_format='{l_bar}{bar}| {elapsed}<{remaining}{postfix}')
 
+        # create shared arrays to be passed to processes
+        s_volumes, volumes = create_shared_array(
+            np.zeros(self.frac_points.shape[0]), return_buffer=True)
+        s_groups_idx = create_shared_array(self._groups_idx)
+        s_periodic_points_cart = create_shared_array(self._periodic_points_cart)
+
+        # spawn as many worker processes as needed, put all bands in the queue,
+        # and let them work until all the required rates have been computed.
+        workers = []
+        iqueue = Queue()
+        oqueue = Queue()
+
+        for i in range(self._nworkers):
+            workers.append(Process(
+                target=voronoi_worker,
+                args=(s_volumes, s_groups_idx, s_periodic_points_cart,
+                      len(self._periodic_points),
+                      len(self.frac_points), iqueue, oqueue)))
+            workers[-1].start()
+
+        n_blocks_to_run = 0
         for nx, ny, nz in np.ndindex(tuple(self._n_blocks_by_axis + 2)):
             # group numbers start from 1
             nx += 1
             ny += 1
             nz += 1
 
-            if nx in b_idx[0] or ny in b_idx[1] or nz in b_idx[2]:
+            if nx in buffer[0] or ny in buffer[1] or nz in buffer[2]:
                 # cell is in the buffer zone outside the unitcell therefore
                 # don't calculate voronoi weights
                 continue
 
-            # get the indices of the block we are interested in
-            block_idx = self._get_idx_by_group(nx, ny, nz)
+            iqueue.put((nx, ny, nz))
+            n_blocks_to_run += 1
 
-            # get the indices of the points to include when calculating the
-            # Voronoi diagram, this includes the block of interest and the
-            # blocks immediately surrounding it
-            voro_idx = self._get_idx_by_group(
-                (nx - 1, nx + 1), (ny - 1, ny + 1), (nz - 1, nz + 1))
+        for _ in range(n_blocks_to_run):
+            pbar.update(oqueue.get())
 
-            # get the indices of the block points in voro_idx, I.e.
-            # it is the index of the point in voro_idx, not the index of the
-            # point in the original periodic mesh. Allows us to map from the
-            # voro results to the original periodic mesh
-            voro_to_block_idx = _get_loc(voro_idx, block_idx)
+        # Nones signal workers to stop
+        for i in range(self._nworkers):
+            iqueue.put(None)
 
-            # Now get the indices of the unit cell points (in the periodic mesh)
-            # that we are including in the voronoi diagram, so we can calculate
-            # the volumes for just these points
-            points_in_voro = voro_idx[voro_to_block_idx]
+        # Run workers till all data has been collected
+        for worker in workers:
+            worker.join()
+            worker.terminate()
 
-            # finally, normalise these indices to get their corresponding index
-            # in the original frac_points mesh. As we put the original
-            # frac_points at the end of the periodic_points mesh this is easy.
-            points_in_voro -= len(self._periodic_points) - len(self.frac_points)
+        zero_vols = volumes == 0
+        inf_vols = volumes == np.inf
+        if any(zero_vols):
+            logger.warning("{} volumes are zero".format(np.sum(zero_vols)))
 
-            voro = Voronoi(self._periodic_points_cart[voro_idx])
-            volumes[points_in_voro] = _get_voronoi_volumes(
-                voro, voro_to_block_idx)
-
-            pbar.update(len(points_in_voro))
+        if any(inf_vols):
+            logger.warning("{} volumes are infinite".format(np.sum(inf_vols)))
 
         return volumes
 
-    def _get_idx_by_group(self,
-                          nx: Union[int, Tuple[int, int]],
-                          ny: Union[int, Tuple[int, int]],
-                          nz: Union[int, Tuple[int, int]]):
-        if isinstance(nx, tuple):
-            xmask = ((self._groups_idx[0] >= nx[0]) &
-                     (self._groups_idx[0] <= nx[1]))
-        else:
-            xmask = self._groups_idx[0] == nx
 
-        if isinstance(ny, tuple):
-            ymask = ((self._groups_idx[1] >= ny[0]) &
-                     (self._groups_idx[1] <= ny[1]))
-        else:
-            ymask = self._groups_idx[1] == ny
+def voronoi_worker(s_volumes, s_groups_idx, s_periodic_points_cart,
+                   n_periodic_points, n_frac_points, iqueue, oqueue):
+    volumes = np.frombuffer(s_volumes)
+    groups_idx = np.frombuffer(s_groups_idx).reshape(3, -1)
+    periodic_points_cart = np.frombuffer(s_periodic_points_cart).reshape(-1, 3)
+    periodic_idx = np.arange(n_periodic_points)
+    n_buffer_points = n_periodic_points - n_frac_points
 
-        if isinstance(nz, tuple):
-            zmask = ((self._groups_idx[2] >= nz[0]) &
-                     (self._groups_idx[2] <= nz[1]))
-        else:
-            zmask = self._groups_idx[2] == nz
+    while True:
+        job = iqueue.get()
 
-        return self._periodic_idx[xmask & ymask & zmask]
+        if job is None:
+            break
+
+        nx, ny, nz = job
+
+        # get the indices of the block we are interested in
+        block_idx = _get_idx_by_group(groups_idx, periodic_idx, nx, ny, nz)
+
+        # get the indices of the points to include when calculating the
+        # Voronoi diagram, this includes the block of interest and the
+        # blocks immediately surrounding it
+        voro_idx = _get_idx_by_group(
+            groups_idx, periodic_idx, (nx - 1, nx + 1), (ny - 1, ny + 1),
+            (nz - 1, nz + 1))
+
+        # get the indices of the block points in voro_idx, I.e.
+        # it is the index of the point in voro_idx, not the index of the
+        # point in the original periodic mesh. Allows us to map from the
+        # voro results to the original periodic mesh
+        voro_to_block_idx = _get_loc(voro_idx, block_idx)
+
+        # Now get the indices of the unit cell points (in the periodic mesh)
+        # that we are including in the voronoi diagram, so we can calculate
+        # the volumes for just these points
+        points_in_voro = voro_idx[voro_to_block_idx]
+
+        # finally, normalise these indices to get their corresponding index
+        # in the original frac_points mesh. As we put the original
+        # frac_points at the end of the periodic_points mesh this is easy.
+        points_in_voro -= n_buffer_points
+
+        voro = Voronoi(periodic_points_cart[voro_idx])
+
+        volumes[points_in_voro] = _get_voronoi_volumes(voro, voro_to_block_idx)
+        oqueue.put(len(points_in_voro))
+
+
+def _get_idx_by_group(groups_idx,
+                      periodic_idx,
+                      nx: Union[int, Tuple[int, int]],
+                      ny: Union[int, Tuple[int, int]],
+                      nz: Union[int, Tuple[int, int]]):
+    xgroups = groups_idx[0]
+    ygroups = groups_idx[1]
+    zgroups = groups_idx[2]
+
+    if isinstance(nx, tuple):
+        xmin = nx[0]
+        xmax = nx[1]
+        evaluate_str = "(xgroups >= xmin) & (xgroups <= xmax)"
+    else:
+        evaluate_str = "(xgroups == nx)"
+
+    if isinstance(ny, tuple):
+        ymin = ny[0]
+        ymax = ny[1]
+        evaluate_str += " & (ygroups >= ymin) & (ygroups <= ymax)"
+    else:
+        evaluate_str += " & (ygroups == ny)"
+
+    if isinstance(nz, tuple):
+        zmin = nz[0]
+        zmax = nz[1]
+        evaluate_str += " & (zgroups >= zmin) & (zgroups <= zmax)"
+    else:
+        evaluate_str += " & (zgroups == nz)"
+
+    mask = ne.evaluate(evaluate_str)
+    return periodic_idx[mask]
 
 
 def _get_voronoi_volumes(voronoi, volume_indices) -> np.ndarray:
-    volumes = np.zeros(len(volume_indices))
 
-    for i, reg_num in enumerate(voronoi.point_region[volume_indices]):
-        indices = voronoi.regions[reg_num]
+    def get_volume(indices):
         if -1 in indices:
-            # some regions can be opened
-            volumes[i] = np.inf
+            # some regions can be open
+            return np.inf
         else:
-            volumes[i] = ConvexHull(voronoi.vertices[indices]).volume
-    return volumes
+            return ConvexHull(voronoi.vertices[indices]).volume
+
+    return np.array([get_volume(voronoi.regions[x])
+                     for x in voronoi.point_region[volume_indices]])
 
 
 def _get_loc(x, y):
