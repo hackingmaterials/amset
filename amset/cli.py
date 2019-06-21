1fa4632973af25047509d01ca8da6123f88d2d29
Alex Ganose <alexganose@googlemail.com>
2019-06-21 11:26:09 -0700

    Add command line script
    Former-commit-id: 9394708451c918cc9853cebb4d51778e7f7e7d81 [formerly 518588e24658f0a3e691d127091b359532703c9f] [formerly 6cacf5bad122c38b481282c4b80f3d3415525781 [formerly ed6bdd7a57980131a668794650f32dff4c2a447e]] [formerly bea288b4b3cfa8ad755f0886818e3b1c04034d84 [formerly 3776f74d48a29627ff8c9ff6ff6748f9e1eba41d] [formerly cacceea08b9960687afe79b63cb04201533a1d00 [formerly 9530348afd89e8e0de19556aa822626a3bb2f34e]]]
    Former-commit-id: 72a588af6f3b9eeb323be71fb5af79998956224a [formerly 3819e99f60cee83c6c0e941af28c7625531b0630] [formerly 8968055f103dfb0041274aa19a159e53dc9291b4 [formerly 73c38425fbd494bbb22ced6ed71659d47110d759]]
    Former-commit-id: be17eae382771b22ffc0bea0a35c5ca0b3bc16a6 [formerly 1dab592239f19ef8df8be4a71c4dec07d9689f1c]
    Former-commit-id: e183f99ea1fdd4e5ddb01e4043fcabb6da2950d3

--- amset/cli.py
+++ amset/cli.py
@@ -3,174 +3,331 @@ This module contains a script for using amset from the command line.
 """
 
 import argparse
-import logging
-import sys
 import warnings
-from typing import Optional
 
-from pymatgen.core.structure import Structure
-from amset import __version__
+from collections import defaultdict
+from typing import Dict, Any
+
+from amset.log import initialize_amset_logger
+from amset.run import AmsetRunner
+from amset import __version__, amset_defaults
+from amset.util import parse_deformation_potential, parse_temperatures, \
+    parse_doping
 
 __author__ = "Alex Ganose"
 __maintainer__ = "Alex Ganose"
 __email__ = "aganose@lbl.gov"
-__date__ = "December 17, 2018"
+__date__ = "June 21, 2019"
 
 
-def amset(structure: Structure,
-                         condenser_kwargs: Optional[dict] = None,
-                         describer_kwargs: Optional[dict] = None,
-                         ) -> str:
-    """Gets the robocrystallographer description of a structure.
+def main():
+    warnings.filterwarnings("ignore", category=UserWarning,
+                            module="pymatgen")
+    warnings.filterwarnings("ignore", category=RuntimeWarning,
+                            module="amset")
+    warnings.filterwarnings("ignore", category=FutureWarning,
+                            module="scipy")
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-    Args:
-        structure: A structure.
-        condenser_kwargs: Keyword arguments that will be passed to
-            :obj:`robocrys.condense.StructureCondenser`.
-        describer_kwargs: Keyword arguments that will be passed to
-            :obj:`robocrys.describe.StructureDescriber`.
+    args = _get_parser().parse_args()
+    args_dict = vars(args)
 
-    Returns:
-        The description.
-    """
-    condenser_kwargs = condenser_kwargs if condenser_kwargs else {}
-    describer_kwargs = describer_kwargs if describer_kwargs else {}
+    if args.print_log is not False:
+        initialize_amset_logger()
 
-    sc = StructureCondenser(**condenser_kwargs)
-    describer = StructureDescriber(**describer_kwargs)
+    settings_override: Dict[str, Dict[str, Any]] = defaultdict(dict)
+    for section in amset_defaults:
+        for setting in amset_defaults[section]:
+            if setting in args_dict and args_dict[setting] is not None:
+                settings_override[section][setting] = args_dict[setting]
 
-    try:
-        logging.info("Adding oxidation states...")
-        structure.add_oxidation_state_by_guess(max_sites=-80)
-    except ValueError:
-        logging.warning("Could not add oxidation states!")
+    runner = AmsetRunner.from_directory(
+        directory=args.directory,
+        vasprun=args.vasprun,
+        settings_file=args.settings,
+        settings_override=settings_override)
 
-    condensed_structure = sc.condense_structure(structure)
-    description = describer.describe(condensed_structure)
-    logging.info(description)
-    return description
+    runner.run()
 
 
 def _get_parser():
     parser = argparse.ArgumentParser(
-        description="amset is a tool to calculate carrier transport properties "
-                    "from ab initio calculations",
+        description="AMSET is a tool to calculate carrier transport properties "
+                    "from ab initio calculation data",
         epilog="Author: {}, Version: {}, Last updated: {}".format(
             __author__, __version__, __date__))
 
-    parser.add_argument('filename',
-                        help="structure file or mpid")
-    parser.add_argument('-c', '--conventional',
-                        dest="use_conventional_cell",
-                        action='store_true',
-                        help="use the convention cell")
-    parser.add_argument('-s', '--symmetry',
-                        action='store_true',
-                        dest="use_symmetry",
-                        help="use symmetry to determine inequivalent sites")
-    parser.add_argument('--symprec',
-                        default=0.01,
-                        help="symmetry tolerance")
-    parser.add_argument('--no-simplify',
-                        action='store_false',
-                        dest="simplify_molecules",
-                        help="don't simplify molecules when mineral matching")
-    parser.add_argument('--no-iupac',
-                        action="store_false",
-                        dest="use_iupac_formula",
-                        help="don't use IUPAC formula ordering")
-    parser.add_argument('--no-common-formulas',
-                        dest="use_common_formulas",
-                        action="store_false",
-                        help="don't use common formulas")
-    parser.add_argument('--no-mineral',
-                        dest="describe_mineral",
-                        action="store_false",
-                        help="don't describe the mineral information")
-    parser.add_argument('--no-makeup',
-                        dest="describe_component_makeup",
-                        action="store_false",
-                        help="don't describe the component makeup")
-    parser.add_argument('--no-components',
-                        dest="describe_components",
-                        action="store_false",
-                        help="don't describe the components")
-    parser.add_argument('--no-symmetry-labels',
-                        dest="describe_symmetry_labels",
-                        action="store_false",
-                        help="don't describe symmetry labels")
-    parser.add_argument('--no-oxi',
-                        dest="describe_oxidation_states",
-                        action="store_false",
-                        help="don't describe oxidation states")
-    parser.add_argument('--no-bond',
-                        dest="describe_bond_lengths",
-                        action="store_false",
-                        help="don't describe bond lengths")
-    parser.add_argument('--precision',
-                        metavar="P",
-                        dest="bond_length_decimal_places",
-                        default=2,
-                        help="decimal places for bond lengths")
-    parser.add_argument('--distorted-tol',
-                        metavar="T",
-                        dest="distorted_tol",
-                        default=0.6,
-                        help="order parameter below which sites are distorted")
-    parser.add_argument('--anion-polyhedra',
-                        dest="only_describe_cation_polyhedra_connectivity",
-                        action="store_true",
-                        help="describe anion polyhedra connectivity")
-    parser.add_argument('--verbose-bonds',
-                        dest="only_describe_bonds_once",
-                        action="store_false",
-                        help="describe bond lengths for each site")
-    parser.add_argument('--latexify',
-                        action="store_true",
-                        help="format the description for use in LaTeX")
+    parser.add_argument(
+        '--directory',
+        metavar="D",
+        default='.',
+        help="path to directory with vasprun and settings files"
+    )
+
+    parser.add_argument(
+        '-v',
+        '--vasprun',
+        metavar="V",
+        default=None,
+        help="path to vasprun.xml file"
+    )
+
+    parser.add_argument(
+        '-s',
+        '--settings',
+        metavar="S",
+        default=None,
+        help="path to settings file"
+    )
+
+    parser.add_argument(
+        '-i',
+        '--interpolation-factor',
+        metavar="F",
+        default=None,
+        type=float,
+        help="band structure interpolation factor"
+    )
+
+    parser.add_argument(
+        '-n',
+        '--num-extra-kpoints',
+        metavar="N",
+        default=None,
+        type=float,
+        help="number of additional k-points to add around the Fermi level"
+    )
+
+    parser.add_argument(
+        '--d',
+        "--doping",
+        metavar="D",
+        default=None,
+        type=parse_doping,
+        help='doping concentrations (e.g. "1E13,1E14" or "1E13:1E20:8")'
+    )
+
+    parser.add_argument(
+        '--t',
+        "--temperatures",
+        metavar="T",
+        default=None,
+        type=parse_temperatures,
+        help='temperatures (e.g. "300,400" or "300:1000:8")'
+    )
+
+    parser.add_argument(
+        '--scissor',
+        metavar="S",
+        default=None,
+        type=float,
+        help='amount to scissor the band gap')
+
+    parser.add_argument(
+        '--bandgap',
+        metavar="B",
+        default=None,
+        type=float,
+        help="automatically set scissor to make the band gap this value"
+    )
+
+    parser.add_argument(
+        '--scattering-type',
+        metavar="M",
+        default=None,
+        type=float,
+        help="scattering mechanisms to include [default: auto]"
+    )
+
+    parser.add_argument(
+        "--high-frequency-dielectric",
+        metavar="ε_∞",
+        default=None,
+        type=float,
+        help='high-frequency dielectric constant'
+    )
+
+    parser.add_argument(
+        "--static-dielectric",
+        metavar="ε_s",
+        default=None,
+        type=float,
+        help='static dielectric constant'
+    )
+
+    parser.add_argument(
+        "--elastic-constant",
+        metavar="C",
+        default=None,
+        type=float,
+        help='elastic constant [GPa]'
+    )
+
+    parser.add_argument(
+        "--deformation-potential",
+        metavar="E_D",
+        default=None,
+        type=parse_deformation_potential,
+        help='deformation potential [eV] (e.g. "7.4" or "7.4,6.8")'
+    )
+
+    parser.add_argument(
+        "--piezoelectric-constant",
+        metavar="P",
+        default=None,
+        type=float,
+        help='piezoelectric constant'
+    )
+
+    parser.add_argument(
+        "--acceptor-charge",
+        metavar="C",
+        default=None,
+        type=float,
+        help='acceptor defect charge'
+    )
+
+    parser.add_argument(
+        "--donor-charge",
+        metavar="C",
+        default=None,
+        type=float,
+        help='donor defect charge'
+    )
+
+    parser.add_argument(
+        "--pop-frequency",
+        metavar="ω",
+        default=None,
+        type=float,
+        help='polar optical phonon frequency [THz]'
+    )
+
+    parser.add_argument(
+        "--gauss-width",
+        metavar="W",
+        default=None,
+        type=float,
+        help='gaussian width used to evaluate scattering delta [eV]'
+    )
+
+    parser.add_argument(
+        "--energy-cutoff",
+        metavar="E",
+        default=None,
+        type=float,
+        help='energy cut-off for band interpolation [eV]'
+    )
+
+    parser.add_argument(
+        "--fd-tol",
+        metavar="T",
+        default=None,
+        type=float,
+        help="Fermi-Dirac tolerance below which scattering rates are not "
+             "calculated [%%]"
+    )
+
+    parser.add_argument(
+        "--g-tol",
+        metavar="T",
+        default=None,
+        type=float,
+        help="convergence tolerance for IBTE solution [%%]"
+    )
+
+    parser.add_argument(
+        "--max-g-iter",
+        metavar="N",
+        default=None,
+        type=int,
+        help="maximum number of iterations for solving g"
+    )
+
+    parser.add_argument(
+        "--dos-estep",
+        metavar="E",
+        default=None,
+        type=float,
+        help="dos energy step [eV]"
+    )
+
+    parser.add_argument(
+        "--dos-width",
+        metavar="W",
+        default=None,
+        type=float,
+        help="dos smearing width [eV]"
+    )
+
+    parser.add_argument(
+        "--symprec",
+        metavar="S",
+        default=None,
+        type=float,
+        help="symmetry precision"
+    )
+
+    parser.add_argument(
+        "--nworkers",
+        metavar="N",
+        default=None,
+        type=float,
+        help="number of processors to use"
+    )
+
+    parser.add_argument(
+        "--no-calculate-mobility",
+        default=None,
+        dest="calculate_mobility",
+        action="store_false",
+        help="don't calculate mobility"
+    )
+
+    parser.add_argument(
+        "--no-separate-mobility",
+        default=None,
+        dest="separate_scattering_mobilities",
+        action="store_false",
+        help="don't separate the individual scattering rate mobilities"
+    )
+
+    parser.add_argument(
+        "--file-format",
+        metavar="F",
+        default=None,
+        help="output file format [options: json, yaml, txt, dat]"
+    )
+
+    parser.add_argument(
+        "--write-input",
+        default=None,
+        action="store_true",
+        help="write input settings to disk"
+    )
+
+    parser.add_argument(
+        "--write-mesh",
+        default=None,
+        action="store_true",
+        help="write mesh data, including band energies and scattering rates"
+    )
+
+    parser.add_argument(
+        "--no-log",
+        default=None,
+        dest="print_log",
+        action="store_false",
+        help="don't print log messages"
+    )
+
+    parser.add_argument(
+        "--log-error-traceback",
+        default=None,
+        action="store_true",
+        help="log full error message tracebacks"
+    )
+
     return parser
 
 
-def main():
-    args = _get_parser().parse_args()
-    args_dict = vars(args)
-
-    condenser_keys = ['use_conventional_cell', "use_symmetry", "symprec",
-                      "use_iupac_formula", "use_common_formulas"]
-    describer_keys = ['describe_mineral', "describe_component_makeup",
-                      "describe_components", "describe_symmetry_labels",
-                      "describe_oxidation_states", "describe_bond_lengths",
-                      "bond_length_decimal_places", "distorted_tol",
-                      "only_describe_cation_polyhedra_connectivity",
-                      "only_describe_bonds_once", "latexify"]
-
-    condenser_kwargs = {key: args_dict[key] for key in condenser_keys}
-    describer_kwargs = {key: args_dict[key] for key in describer_keys}
-
-    logging.basicConfig(filename='robocrys.log', level=logging.INFO,
-                        filemode='w', format='%(message)s')
-    console = logging.StreamHandler()
-    logging.info(" ".join(sys.argv[:]))
-    logging.getLogger('').addHandler(console)
-
-    warnings.filterwarnings("ignore", category=UserWarning,
-                            module="pymatgen")
-    warnings.filterwarnings("ignore", category=DeprecationWarning)
-
-    try:
-        structure = Structure.from_file(args.filename)
-
-    except FileNotFoundError:
-        from pymatgen.ext.matproj import MPRester
-
-        mpr = MPRester()
-
-        try:
-            structure = mpr.get_entry_by_material_id(
-                args.filename, inc_structure='final').structure
-        except IndexError:
-            logging.error("filename or mp-id not found.")
-            sys.exit()
-
-    robocrystallographer(structure, condenser_kwargs=condenser_kwargs,
-                         describer_kwargs=describer_kwargs)
