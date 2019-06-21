af152e7a1aec4302df2241a690058ee3c215c37f
Alex Ganose <alexganose@googlemail.com>
2019-06-20 23:21:59 -0700

    Add custom k-point weights to transport property calculations

--- amset/transport.py
+++ amset/transport.py
@@ -98,22 +98,22 @@ def _calculate_mobility(amset_data: AmsetData,
         # mask data to remove extra k-points that are not part of the regular
         # k-point mesh (necessary as BoltzTraP2 doesn't support custom k-point
         # weights.
-        energies = energies[:, kmask]
-        vv = vv[..., kmask]
-        lifetimes = lifetimes[:, kmask]
+        # energies = energies[:, kmask]
+        # vv = vv[..., kmask]
+        # lifetimes = lifetimes[:, kmask]
 
         # Nones are required as BoltzTraP2 expects the Fermi and temp as arrays
         fermi = amset_data.fermi_levels[n, t][None] * units.eV
         temp = amset_data.temperatures[t][None]
 
         # obtain the Fermi integrals for the temperature and doping
-        epsilon, dos, vvdos, cdos = BTPDOS(
-            energies, vv, scattering_model=lifetimes,
-            npts=len(amset_data.dos.energies))
-        # epsilon, dos, vvdos, cdos = AMSETDOS(
+        # epsilon, dos, vvdos, cdos = BTPDOS(
         #     energies, vv, scattering_model=lifetimes,
-        #     npts=len(amset_data.dos.energies),
-        #     kpoint_weights=amset_data.kpoint_weights)
+        #     npts=len(amset_data.dos.energies))
+        epsilon, dos, vvdos, cdos = AMSETDOS(
+            energies, vv, scattering_model=lifetimes,
+            npts=len(amset_data.dos.energies),
+            kpoint_weights=amset_data.kpoint_weights)
 
         _, l0, l1, l2, lm11 = fermiintegrals(
             epsilon, dos, vvdos, mur=fermi, Tr=temp,
@@ -145,8 +145,8 @@ def _calculate_transport_properties(amset_data):
     # mask data to remove extra k-points that are not part of the regular
     # k-point mesh (necessary as BoltzTraP2 doesn't support custom k-point
     # weights.
-    energies = energies[:, kmask]
-    vv = vv[..., kmask]
+    # energies = energies[:, kmask]
+    # vv = vv[..., kmask]
 
     n_t_size = (len(amset_data.doping), len(amset_data.temperatures))
 
@@ -158,22 +158,23 @@ def _calculate_transport_properties(amset_data):
     for n, t in np.ndindex(n_t_size):
         sum_rates = [np.sum(amset_data.scattering_rates[s][:, n, t], axis=0)
                      for s in amset_data.spins]
+        print(np.array(sum_rates).min())
         lifetimes = 1 / np.vstack(sum_rates)
-        lifetimes = lifetimes[:, kmask]
-        print(lifetimes.min())
+        # lifetimes = lifetimes[:, kmask]
+        # print(lifetimes.min())
 
         # Nones are required as BoltzTraP2 expects the Fermi and temp as arrays
         fermi = amset_data.fermi_levels[n, t][None] * units.eV
         temp = amset_data.temperatures[t][None]
 
         # obtain the Fermi integrals
-        epsilon, dos, vvdos, cdos = BTPDOS(
-            energies, vv, scattering_model=lifetimes,
-            npts=len(amset_data.dos.energies))
-        # epsilon, dos, vvdos, cdos = AMSETDOS(
+        # epsilon, dos, vvdos, cdos = BTPDOS(
         #     energies, vv, scattering_model=lifetimes,
-        #     npts=len(amset_data.dos.energies),
-        #     kpoint_weights=amset_data.kpoint_weights)
+        #     npts=len(amset_data.dos.energies))
+        epsilon, dos, vvdos, cdos = AMSETDOS(
+            energies, vv, scattering_model=lifetimes,
+            npts=len(amset_data.dos.energies),
+            kpoint_weights=amset_data.kpoint_weights)
 
         _, l0, l1, l2, lm11 = fermiintegrals(
             epsilon, dos, vvdos, mur=fermi, Tr=temp,
