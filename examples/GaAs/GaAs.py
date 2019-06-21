5641de1c0c86b87a4a97c4197c9cebb8ec36a157
Alex Ganose <alexganose@googlemail.com>
2019-06-21 11:26:44 -0700

    Update the examples
    Former-commit-id: 85691bfb6e427393e748601e03d9306825ac8f0e [formerly bb21bf84f2eed6a678a74046657da3496160cd3b] [formerly 6420ba109eb8b66ce4d3da73e955912eb0f7a21f [formerly c40cbed5dbcb350ad23707ddd87dbe48d1702271]] [formerly 64ec2d667faad3f86ac31c743637e9dbce4151b1 [formerly 45b317102b552fddb2b6bf8f25f41055535497a0] [formerly b101d674abe853a0f155396f31b12315be5d870f [formerly 7db99dde41408c32abb2e4711813c00fca0ed977]]]
    Former-commit-id: 3de06b7c33099703e10f0fcd3f9620484b28301a [formerly c3c781c64ea7b735fa8e75e39dc1425aa2fff98c] [formerly 67db1c5cef36e949ad784adda077f15a078519cb [formerly 7b32568cc8fbc07cc3cf64d18f9589cdd5ba88fa]]
    Former-commit-id: b696b098cbefea65fe2f700a1020ccfceb5898f4 [formerly 2b82efa8ec7e13cd317f0c0014d0c9c8732a1b81]
    Former-commit-id: 125a7be0207c0ba7f975efa8885b787d95adfc38

--- examples/GaAs/GaAs.py
+++ examples/GaAs/GaAs.py
@@ -1,17 +1,17 @@
 import warnings
 
-from amset.log import initialize_amset_logger
 from amset.run import AmsetRunner
 
 settings = {
     "general": {
         "interpolation_factor": 450,
-        # "interpolation_factor": 100,
         "scattering_type": "auto",
         "doping": [3e13],
         "temperatures": [201, 290, 300, 401, 506, 605, 789, 994],
-        "bandgap": 1.33
+        "bandgap": 1.33,
+        "n_extra_kpoints": 5000
     },
+
     "material": {
         "deformation_potential": (8.6, 8.6),
         "elastic_constant": 139.7,
@@ -21,26 +21,16 @@ settings = {
         "high_frequency_dielectric": 10.32,
         "pop_frequency": 8.16
     },
-    "performance": {
-        "energy_tol": 0.005,
-        "energy_cutoff": 1.5,
-        "n_extra_kpoints": 5000,
-        "g_tol": 1e-5,
-        "max_g_iter": 1,
-        "symprec": 0.01,
-        "nworkers": -1,
-        "dos_estep": 0.001,
-    },
+
     "output": {
         "separate_scattering_mobilities": False,
         "log_traceback": True,
-        "write_mesh": False
+        "print_log": True
     }
 }
 
 warnings.simplefilter("ignore")
 
-initialize_amset_logger(log_traceback=settings["output"]["log_traceback"])
 runner = AmsetRunner.from_vasprun_and_settings("vasprun.xml.gz", settings)
 runner.run()
 
--- examples/Si/Si.py
+++ examples/Si/Si.py
@@ -1,16 +1,16 @@
 import warnings
 
-from amset.log import initialize_amset_logger
 from amset.run import AmsetRunner
 
 settings = {
     "general": {
-        "interpolation_factor": 5,
+        "interpolation_factor": 150,
         "scattering_type": "auto",
         "doping": [1.99e+14, 2.20e+15, 1.72e+16,
                    1.86e+17, 1.46e+18, 4.39e+18],
         "temperatures": [300]
     },
+
     "material": {
         "deformation_potential": (6.5, 6.5),
         "elastic_constant": 190,
@@ -20,28 +20,21 @@ settings = {
         "high_frequency_dielectric": 13.1,
         "pop_frequency": 15.23,
     },
+
     "performance": {
-        "energy_tol": 0.001,
-        "n_extra_kpoints": 25000,
+        "gauss_width": 0.001,
         "energy_cutoff": 1.5,
-        "g_tol": 1e-5,
-        "max_g_iter": 5,
         "symprec": 0.01,
         "nworkers": -1,
         "dos_estep": 0.01,
-        "dos_width": 0.01
     },
+
     "output": {
-        # "separate_scattering_mobilities": False,
-        "log_traceback": True,
-        "write_mesh": True
+        "print_log": True,
     }
 }
 
 warnings.simplefilter("ignore")
 
-initialize_amset_logger(log_traceback=settings["output"]["log_traceback"])
-
 runner = AmsetRunner.from_vasprun_and_settings("vasprun.xml.gz", settings)
-
 runner.run()
