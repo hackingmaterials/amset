442c9b5288f61dde78248392c2d0e6c77ca38b65
Alex Ganose <alexganose@googlemail.com>
2019-11-13 12:41:11 -0800

    Renable curvature

--- amset/interpolation/densify.py
+++ amset/interpolation/densify.py
@@ -231,12 +231,12 @@ class BandDensifier(object):
         # have to use amset_data scissor and not the user specified band gap,
         # as there is no guarantee that the extra k-points will go through the
         # CBM & VBM
-        energies, vvelocities, projections, mapping_info = self._interpolater.get_energies(
+        energies, vvelocities, curvature, projections, mapping_info = self._interpolater.get_energies(
             additional_kpoints,
             energy_cutoff=self._energy_cutoff,
             scissor=self._amset_data.scissor / units.eV,
             return_velocity=True,
-            return_curvature=False,
+            return_curvature=True,
             return_projections=True,
             atomic_units=True,
             return_vel_outer_prod=True,
@@ -268,7 +268,7 @@ class BandDensifier(object):
             kpoint_weights,
             mapping_info["ir_kpoints_idx"],
             mapping_info["ir_to_full_idx"],
-            # curvature,
+            curvature,
         )
 
 
