5420c1b898b642bfce80306781f015024480e117
Alex Ganose <alexganose@googlemail.com>
2019-11-13 12:43:44 -0800

    Minor bug fixes for Voronoi

--- amset/interpolation/tests/test_voronoi.py
+++ amset/interpolation/tests/test_voronoi.py
@@ -4,18 +4,64 @@ import numpy as np
 
 
 from amset.interpolation.voronoi import PeriodicVoronoi
-from amset.misc.util import get_dense_kpoint_mesh_spglib
+from amset.kpoints import kpoints_to_first_bz, get_dense_kpoint_mesh_spglib
 from pymatgen import Lattice
 
 test_dir = os.path.dirname(os.path.abspath(__file__))
-amset_files = os.path.join(test_dir, '..', '..', '..', 'test_files',
-                           'AlCuS2_mp-4979')
+amset_files = os.path.join(test_dir, "..", "..", "..", "test_files", "AlCuS2_mp-4979")
 
 
 class TestPeriodicVoronoi(unittest.TestCase):
-    """Tests for interpolating a band structure using BoltzTraP2."""
-
     def test_get_volumes(self):
+        # lattice = Lattice([1, 0, 0, 0, 1, 0, 0, 0, 1])
+        lattice = Lattice(
+            [
+                [-1.14892994, 1.14892994, 1.14892994],
+                [1.14892994, -1.14892994, 1.14892994],
+                [1.14892994, 1.14892994, -1.14892994],
+            ]
+        )
+
+        mesh = np.array([3, 3, 3])
+        kpoints = get_dense_kpoint_mesh_spglib(mesh)
+        # extra_points = get_dense_kpoint_mesh_spglib([2, 1, 1]) / mesh + [-0.4, -0.4, -0.4]
+        # print(extra_points)
+        # extra_points = kpoints_to_first_bz(extra_points)
+
+        # extra_points = np.array([[-0.4, -0.4, -0.4], [-0.23333333, -0.4, -0.4]])
+        extra_points = np.array([[-0.4, -0.4, -0.4]])
+        all_points = np.concatenate((kpoints, extra_points))
+
+        # extra_points = np.array([[0.49, 0.49, 0.49]]) #, [-0.49, 0.49, 0.49]])
+        # extra_points = np.array([[0.49, 0.49, 0.49], [-0.49, -0.49, -0.49]])
+        # extra_points = np.array([[-0.4, -0.4, 0.], [0.4, 0.4, 0.0]])
+        # [-0.49, -0.49, -0.1], [-0.49, 0.49, 0],
+        # [-0.49, 0.49, 0.1], [-0.49, 0.49, -0.1],
+        # [0.49, -0.49, 0], [0.49, -0.49, 0.1],
+        # [0.49, -0.49, -0.1], [0.49, 0.49, 0.],
+        # [0.49, 0.49, 0.1], [0.49, 0.49, -0.1],
+        # ])
+        # extra_points = np.array([[-0.49, -0.49, 0.], [-0.49, -0.49, 0.1],
+        #                          [-0.49, -0.49, -0.1], [-0.49, 0.49, 0],
+        #                          [-0.49, 0.49, 0.1], [-0.49, 0.49, -0.1],
+        #                          [0.49, -0.49, 0], [0.49, -0.49, 0.1],
+        #                          [0.49, -0.49, -0.1], [0.49, 0.49, 0.],
+        #                          [0.49, 0.49, 0.1], [0.49, 0.49, -0.1],
+        #                          ])
+
+        # extra_points = np.array([[0.49, 0.49, 0.49], [-0.49, 0.49, 0.49],
+        #                          [0.49, -0.49, 0.49], [0.49, 0.49, -0.49],
+        #                          [-0.49, -0.49, 0.49], [-0.49, 0.49, -0.49],
+        #                          [0.49, -0.49, -0.49], [-0.49, -0.49, -0.49],
+        #                          ])
+        # extra_points = np.array([[0., 0.0, 0.1], [0, 0, -0.1]])
+        # extra_points = np.array([[0., 0.0, 0.1], [0, 0, -0.1],
+        #                          [0, 0.1, 0], [0, -0.1, 0],
+        #                          [0.1, 0, 0], [-0.1, 0, 0]])
+
+        pv = PeriodicVoronoi(lattice, kpoints, mesh, extra_points)
+        volumes = pv.compute_volumes()
+        print(kpoints)
         # mesh = np.array([100, 100, 100])
         # kpts = get_dense_kpoint_mesh_spglib(mesh, spg_order=True, shift=0.)
         # extra_points = np.array([[0.001, 0, 0], [0.002, 0, 0], [0.003, 0, 0]])
@@ -27,50 +73,48 @@ class TestPeriodicVoronoi(unittest.TestCase):
         # print(all_k[idx][:12])
         # print(vols[idx][:12])
 
-        mesh = np.array([5, 5, 5])
-
-        def get_kpoints(m, shift=0.):
-            k = get_dense_kpoint_mesh_spglib(m, spg_order=True, shift=shift)
-            sort_idx = np.lexsort((k[:, 2], k[:, 1], k[:, 0]))
-            grid = k[sort_idx].reshape(tuple(m) + (3,))
-            grid = grid[:, :, 0].reshape((m[0] * m[1], 3))
-            return grid[:, :2]
-
-        kpts = get_kpoints(mesh)
-
-        dmesh = np.array([5, 5, 5])
-        extra = get_kpoints(dmesh, shift=-0.)
-        extra /= mesh[:2]
-        extra += [0, 0]
-
-        dmesh = np.array([5, 5, 5])
-        lextra = get_kpoints(dmesh, shift=-0.)
-        lextra /= mesh[:2]
-        lextra += [0.2, 0.2]
-
-        dmesh = np.array([3, 3, 3])
-        jextra = get_kpoints(dmesh, shift=-0.)
-        jextra /= mesh[:2]
-        jextra += [0, 0.2]
-
-        dmesh = np.array([7, 7, 7])
-        pextra = get_kpoints(dmesh, shift=-0.)
-        pextra /= mesh[:2]
-        pextra += [-0.20, 0.2]
-
-        kpts = np.concatenate([kpts, extra, lextra, jextra, pextra])
-
-        from scipy.spatial import Voronoi, voronoi_plot_2d
-        vor = Voronoi(kpts)
-        import matplotlib
-        matplotlib.use("TkAgg")
-        import matplotlib.pyplot as plt
-        voronoi_plot_2d(vor)
-
-        plt.xlim((-0.5, 0.5))
-        plt.ylim((-0.5, 0.5))
-        plt.show()
-
+        # def get_kpoints(m, shift=0.):
+        #     k = get_dense_kpoint_mesh_spglib(m, spg_order=True, shift=shift)
+        #     sort_idx = np.lexsort((k[:, 2], k[:, 1], k[:, 0]))
+        #     grid = k[sort_idx].reshape(tuple(m) + (3,))
+        #     grid = grid[:, :, 0].reshape((m[0] * m[1], 3))
+        #     return grid[:, :2]
+        #
+        # kpts = get_kpoints(mesh)
+        #
+        # dmesh = np.array([5, 5, 5])
+        # extra = get_kpoints(dmesh, shift=-0.)
+        # extra /= mesh[:2]
+        # extra += [0, 0]
+        #
+        # dmesh = np.array([5, 5, 5])
+        # lextra = get_kpoints(dmesh, shift=-0.)
+        # lextra /= mesh[:2]
+        # lextra += [0.2, 0.2]
+        #
+        # dmesh = np.array([3, 3, 3])
+        # jextra = get_kpoints(dmesh, shift=-0.)
+        # jextra /= mesh[:2]
+        # jextra += [0, 0.2]
+        #
+        # dmesh = np.array([7, 7, 7])
+        # pextra = get_kpoints(dmesh, shift=-0.)
+        # pextra /= mesh[:2]
+        # pextra += [-0.20, 0.2]
+        #
+        # kpts = np.concatenate([kpts, extra, lextra, jextra, pextra])
+        #
+        # from scipy.spatial import Voronoi, voronoi_plot_2d
+        # vor = Voronoi(kpts)
+        # import matplotlib
+        # matplotlib.use("TkAgg")
+        # import matplotlib.pyplot as plt
+        # voronoi_plot_2d(vor)
+        #
+        # plt.xlim((-0.5, 0.5))
+        # plt.ylim((-0.5, 0.5))
+        # plt.show()
+        #
         # mesh = np.array([
         #     [0, 0, 0][]
         #
@@ -100,5 +144,3 @@ class TestPeriodicVoronoi(unittest.TestCase):
         # plt.xlim((-0.5, 0.5))
         # plt.ylim((-0.5, 0.5))
         # plt.show()
-
-
