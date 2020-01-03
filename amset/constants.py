205930b3bf0bb4cec099e90dc6a36339a24fc1ed
Alex Ganose <alexganose@googlemail.com>
2020-01-03 12:48:22 -0800

    Update amset constants
    Former-commit-id: 39106a33ae74e6aa72df8937acff5d847baa8f90 [formerly 351d0d9182daa72f55227188568b9c9ec2c1a2b8] [formerly 458bd89fc0eab22f5449a1384b52867f6a45205b [formerly 31e6b9fbbcf32702063efe0a5a138c3593a58b28]] [formerly 6f69bc3f187da58549ca91a3f8740a710ac1c568 [formerly 7cd10c673b8bcee036f9bffb2412f671fab4b036] [formerly d0456b2dd0fc0a1d6983567026d237c280ff9f3e [formerly 83c9a7dcef392bb6995aad580687d639da61af83]]]
    Former-commit-id: 3a8715b01a39593261873039290265a97aec9b1a [formerly 156694959c84f08759df1285b00b6989baa2d5cf] [formerly bcc4ff46d1490581524905118643c5e624da8628 [formerly edeeccce75b7c3b4829643cb28f1fde9768994aa]]
    Former-commit-id: 65f538c7f9e62554a5c1a9f64d4c2852bb90fe16 [formerly cd6e5d3742bc84afa30c06eaab6871791462a7a3]
    Former-commit-id: 56ac5bc7260b20aeaeffb986d34015fcf028811f

--- amset/constants.py
+++ amset/constants.py
@@ -4,14 +4,21 @@ from scipy.constants import physical_constants
 
 from pymatgen import Spin
 
+bohr_si = physical_constants["Bohr radius"][0]
+hartree_si = physical_constants["Hartree energy"][0]
+
 hbar = physical_constants["Planck constant over 2 pi in eV s"][0]
 m_e = physical_constants["electron mass"][0]  # in kg
-Ry_to_eV = physical_constants["Rydberg constant times hc in eV"][0]
 hartree_to_ev = physical_constants["Hartree energy in eV"][0]
-bohr_to_angstrom = physical_constants["Bohr radius"][0] * 1e10
+bohr_to_angstrom = bohr_si * 1e10
+bohr_to_cm = bohr_si * 100
+cm_to_bohr = 1 / (bohr_si * 100)
+angstrom_to_bohr = 1 / (bohr_si * 1e10)
+
+gpa_to_au = bohr_si ** 3 / (1e-9 * hartree_si)
+
 A_to_m = 1e-10
 m_to_cm = 100.0
-A_to_nm = 0.1
 e = physical_constants["elementary charge"][0]
 k_B = physical_constants["Boltzmann constant in eV/K"][0]
 default_small_e = 1.0  # eV/cm the value of this parameter does not matter
