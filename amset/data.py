Fix bugs in overlap calculation
Former-commit-id: 8864f9131dc950664d2190fa8487dc30dce1d9de [formerly ef9f65032c6c7256be45883a9fb5a923db1871ac] [formerly ee675bf59178ff3c4124a4307fb1f93c8d51b599 [formerly d91014dc6a6aa4e1e699065df49259815ee846d7]] [formerly 113fa84fd45185be63560a0c51693fd8816f33a0 [formerly 514aa0409fb5207e780ab5439e8951f775c9735d] [formerly a01666a6f67e5003b005af741375adf32ff6fe46 [formerly 34068d354e8dc4a1b2d32a7942dd7cab07b987e0]]]
Former-commit-id: 93179f8de1f210c95e235cddf665d4b7ed3f0f41 [formerly aadcd66d7dc3359ed13888decfdedf9a1d377900] [formerly f0b19e6197862b09a5f2dafa252ed122f69c8fe2 [formerly 14284ce198b17aa003bdd3e8727456ea290a426e]]
Former-commit-id: 1dfc2113985309baf88b629fa32dab5e15e4026f [formerly 4d2b8c88a94dcd9548445df1400d4aada8e57704]
Former-commit-id: 29b0155805d799362ff5a3eca31348c603c41d1a

amset/data.py
@@ -10,6 +10,7 @@ from monty.serialization import dumpfn
 from BoltzTraP2 import units
 from BoltzTraP2.fd import dFDde, FD
 
+from amset.interpolation.overlap import OverlapCalculator
 from amset.tetrahedron import TetrahedralBandStructure
 from pymatgen import Spin, Structure
 
@@ -47,7 +48,7 @@ class AmsetData(MSONable):
         efermi: float,
         is_metal: bool,
         soc: bool,
-        overlap_calculator,
+        overlap_calculator: OverlapCalculator,
         kpoint_weights: Optional[np.ndarray] = None,
         dos: Optional[FermiDos] = None,
         vb_idx: Optional[Dict[Spin, int]] = None,

amset/interpolation/interpolate.py
@@ -44,8 +44,6 @@ from amset.constants import (
     m_to_cm,
     A_to_m,
     hbar,
-    e,
-    m_e,
     bohr_to_angstrom,
     spin_name,
     numeric_types,
@@ -222,6 +220,7 @@ class Interpolater(MSONable):
         effective_mass = {}
         projections = defaultdict(dict)
         new_vb_idx = {}
+        overlap_projections = {}
         for spin in self._spins:
             bands = self._band_structure.bands[spin]
             ibands = np.any((bands > min_e) & (bands < max_e), axis=1)
@@ -267,6 +266,7 @@ class Interpolater(MSONable):
                 )[0]
 
             log_time_taken(t0)
+            overlap_projections[spin] = self._band_structure.projections[spin][ibands]
 
         if is_metal:
             efermi = self._band_structure.efermi * units.eV
@@ -316,7 +316,7 @@ class Interpolater(MSONable):
 
         orig_kpoints = np.array([k.frac_coords for k in self._band_structure.kpoints])
         overlap_calculator = OverlapCalculator(
-            atomic_structure, orig_kpoints, self._band_structure.projections
+            atomic_structure, orig_kpoints, overlap_projections
         )
 
         return AmsetData(
@@ -691,9 +691,6 @@ class Interpolater(MSONable):
             if not atomic_units:
                 energies[spin] = energies[spin] / units.eV
                 velocities[spin] = _convert_velocities(velocities[spin], lattice.matrix)
-
-                if return_curvature:
-                    curvature[spin] = _convert_curvature(curvature[spin])
 
             if return_projections:
                 logger.info("Interpolating {} projections".format(spin_name[spin]))
@@ -1088,19 +1085,6 @@ def _convert_velocities(
     return velocities
 
 
-def _convert_curvature(curvature: np.ndarray) -> np.ndarray:
-    """Convert curvature from atomic units to units of electron rest mass.
-
-    Args:
-        curvature: The band curvature in atomic units.
-
-    Returns:
-        The curvature (i.e., inverse effective mass) in units of 1 / electron rest mass.
-    """
-    factor = bohr_to_angstrom ** 2 * e * hbar ** 2 / (hartree_to_ev * A_to_m ** 2 * m_e)
-    return curvature / factor
-
-
 def _get_projections(projections: np.ndarray) -> Tuple[Tuple[str, np.ndarray], ...]:
     """Extracts and sums the band structure projections for a band.
 
@@ -1110,7 +1094,7 @@ def _get_projections(projections: np.ndarray) -> Tuple[Tuple[str, np.ndarray], .
     Returns:
         The projection labels and orbital projections, as::
 
-            ("s", s_orbital_projections), ("p", p_orbital_projections)
+            ("s", s_orbital_projections), ("p", d_orbital_projections)
     """
     s_orbital = np.sum(projections, axis=3)[:, :, 0]
 

amset/interpolation/overlap.py
@@ -1,20 +1,35 @@
+import logging
+
 import numpy as np
 from scipy.interpolate import RegularGridInterpolator
-from scipy.spatial.distance import cdist
 
 from amset.constants import numeric_types
-from amset.kpoints import expand_kpoints, get_mesh_dim_from_kpoints
+from amset.kpoints import expand_kpoints, get_mesh_dim_from_kpoints, \
+    similarity_transformation
+from pymatgen import Spin
+from pymatgen.util.coord import pbc_diff
+
+_p_orbital_order = [2, 0, 1]  # VASP sorts p orbitals as py, pz, px
+_d_orbital_order = [[4, 0, 3], [0, 4, 1], [3, 1, 2]]
+_select_d_order = ([2, 0, 1, 0, 1], [2, 2, 2, 1, 1])  # selects dz2 dxz dyz dxy dx2
+
+_p_mask = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+_d_mask = np.array([[0, 1, 0, 1, 1], [0, 0, 1, 1, 1], [1, 1, 1, 0, 0]])
+
+logger = logging.getLogger(__name__)
 
 
 class OverlapCalculator(object):
-    def __init__(self, structure, kpoints, projections, distance_function="cosine"):
-        # distance function can be anything supported by the cdist metric argument,
-        # including callable functions
-        self.distance_function = distance_function
+
+    def __init__(self, structure, kpoints, projections):
+
+        logger.info("Initializing orbital overlap calculator")
 
         # k-points have to be on a regular grid, even if only the irreducible part of
-        # the grid is used
-        full_kpoints, ir_to_full_idx = expand_kpoints(structure, kpoints)
+        # the grid is used. If the irreducible part is given, we have to expand it
+        # to the full BZ. Also need to expand the projections to the full BZ using
+        # the rotation mapping
+        full_kpoints, ir_to_full_idx, rot_mapping = expand_kpoints(structure, kpoints)
         mesh_dim = get_mesh_dim_from_kpoints(full_kpoints)
 
         round_dp = int(np.log10(1 / 1e-6))
@@ -36,21 +51,37 @@ class OverlapCalculator(object):
         # TODO: Expand the k-point mesh to account for periodic boundary conditions
         self.interpolators = {}
         for spin, spin_projections in projections.items():
-            nbands, nkpoints = spin_projections.shape[:2]
+            nbands = spin_projections.shape[0]
+            nkpoints = len(ir_to_full_idx)
             nprojections = np.product(spin_projections.shape[2:])
 
-            flat_projections = spin_projections.reshape((nbands, nkpoints, -1))
-            expand_projections = flat_projections[:, ir_to_full_idx]
+            expand_projections = spin_projections[:, ir_to_full_idx]
+            rot_projections = rotate_projections(
+                expand_projections, rot_mapping, structure
+            )
+            # flat_projections = expand_projections.reshape((nbands, nkpoints, -1), order='F')
+            flat_projections = rot_projections.reshape((nbands, nkpoints, -1), order='F')
 
-            # sort the projections then reshape them into the grid. The projections
-            # can now be indexed as projections[iband][ikx][iky][ikz]
-            sorted_projections = expand_projections[:, sort_idx]
+            # aim is to get the wavefunction coefficients
+            # norm_projection = flat_projections / flat_projections.sum(axis=2)[..., None]
+            norm_projection = flat_projections / np.sqrt((flat_projections ** 2).sum(axis=2))[..., None]
+            norm_projection[np.isnan(norm_projection)] = 0
+            # coefficients = np.sqrt(norm_projection)
+            coefficients = norm_projection
+
+            # sort the coefficients then reshape them into the grid. The coefficients
+            # can now be indexed as coefficients[iband][ikx][iky][ikz]
+            sorted_coefficients = coefficients[:, sort_idx]
             grid_shape = (nbands,) + mesh_dim + (nprojections,)
-            grid_projections = sorted_projections.reshape(grid_shape)
+            grid_coefficients = sorted_coefficients.reshape(grid_shape)
+
+            interp_range = (np.arange(nbands), x, y, z)
 
             self.interpolators[spin] = RegularGridInterpolator(
-                (np.arange(nbands), x, y, z), grid_projections, bounds_error=False
+                interp_range, grid_coefficients, bounds_error=False, fill_value=None
             )
+
+        self.rotation_masks = get_rotation_masks(projections)
 
     def get_overlap(self, spin, band_a, kpoint_a, band_b, kpoint_b):
         # k-points should be in fractional
@@ -75,6 +106,18 @@ class OverlapCalculator(object):
         else:
             band_b = np.asarray(band_b)
 
+        # centre = [0.5, 0.5, 0.5]
+        centre = [0., 0, 0]
+
+        shift_a = pbc_diff(kpoint_a, centre)
+        shift_b = pbc_diff(kpoint_b, centre)
+
+        angles = cosine(shift_a, shift_b)
+        # angles = cosine(shift_a, pbc_diff(shift_b, shift_a) + shift_a)
+        angle_weights = np.abs(pbc_diff(kpoint_a, kpoint_b))
+        angle_weights /= np.max(angle_weights, axis=1)[:, None]
+        angle_weights[np.isnan(angle_weights)] = 0
+
         # v2 now has shape of (nkpoints_b, 4)
         v2 = np.concatenate([band_b[:, None], kpoint_b], axis=1)
 
@@ -82,13 +125,136 @@ class OverlapCalculator(object):
         all_v = np.vstack([v1, v2])
 
         # get the interpolate projections for the k-points; p1 is the projections for
-        # kpoint_a, p2 is a list of projections for the kpoint_bs
+        # kpoint_a, p2 is a list of projections for the kpoint_b
         p1, *p2 = self.interpolators[spin](all_v)
 
-        # finally, get the overlap using the specified distance function
-        overlap = 1 - cdist([p1], p2, metric=self.distance_function)[0]
+        # weight the angle masks by the contribution of the transition in that direction
+        weighted_mask = self.rotation_masks[None, ...] * angle_weights[..., None]
+
+        # use the mask to get the angle scaling factor which gives how much the angle
+        # is applied to each projection. I.e., if the scaling factor is 1 for a
+        # particular projection, then the projection is scaled by 1 * the angle. If the
+        # scaling factor is 0.5, the projection is scaled by 0.5 * the angle.
+        # this allows us to only weight specific orbitals
+        scaling_factor = np.max(weighted_mask, axis=1)
+
+        p_product = p1 * p2
+
+        # print("\nka", kpoint_a)
+        # print("\nkb", kpoint_b[0])
+        # print("\npa", p1)
+        # print("\npb", p2[0])
+        # print("\npp", p1 * p2[0])
+        # print(p_product[:2])
+
+        # overlap = np.sum(p_product, axis=1)
+        overlap = np.sum(p_product * (1 - scaling_factor) +
+                         p_product * scaling_factor * angles[:, None], axis=1)
+        # print("\nscale", scaling_factor[:2])
 
         if single_overlap:
-            return overlap[0]
+            return overlap[0] ** 2
         else:
-            return overlap
+            return overlap ** 2
+
+
+def rotate_projections(projections, rotation_mapping, structure):
+    rlat = structure.lattice.reciprocal_lattice.matrix
+
+    similarity_matrix = [similarity_transformation(rlat, r) for r in rotation_mapping]
+    inv_similarity_matrix = [np.linalg.inv(s) for s in similarity_matrix]
+
+    # projections given as (nbands, nkpoints, nprojections, natoms)
+    nprojections = projections.shape[2]
+    rotated_projections = projections.copy()
+
+    if nprojections >= 4:
+        # includes p orbitals
+        rotated_projections[:, :, 1:4, :] = rotate_p_orbitals(
+            projections[:, :, 1:4, :], similarity_matrix
+        )
+
+    if nprojections >= 9:
+        # includes 9 orbitals
+        rotated_projections[:, :, 4:9, :] = rotate_d_orbitals(
+            projections[:, :, 4:9, :], similarity_matrix, inv_similarity_matrix
+        )
+
+    # TODO: Rotate f-orbitals?
+    return rotated_projections
+
+
+def rotate_p_orbitals(p_orbital_projections, similarity_matrix):
+    # p_orbital_projections has the shape (nbands, nkpoints, 3, natoms)
+    # similarity_matrix has the shape (nkpoints, 3, 3)
+
+    # this function both reorders and rotates the p orbitals
+    nbands, nkpoints, _, natoms = p_orbital_projections.shape
+
+    rotated_projections = np.zeros_like(p_orbital_projections)
+    for b_idx, k_idx, a_idx in np.ndindex((nbands, nkpoints, natoms)):
+        rotated_projections[b_idx, k_idx, :, a_idx] = np.dot(
+            similarity_matrix[k_idx],
+            p_orbital_projections[b_idx, k_idx, _p_orbital_order, a_idx]
+        )
+
+    return np.abs(rotated_projections)
+
+
+def rotate_d_orbitals(d_orbital_projections, similarity_matrix, inv_similarity_matrix):
+    # d_orbital_projections has the shape (nbands, nkpoints, 3, natoms)
+    # similarity_matrix has the shape (nkpoints, 3, 3)
+
+    # this function both reorders and rotates the p orbitals
+    nbands, nkpoints, _, natoms = d_orbital_projections.shape
+
+    rotated_projections = np.zeros_like(d_orbital_projections)
+    for b_idx, k_idx, a_idx in np.ndindex((nbands, nkpoints, natoms)):
+        r1 = np.dot(
+            d_orbital_projections[b_idx, k_idx, _d_orbital_order, a_idx],
+            similarity_matrix[k_idx]
+        )
+
+        rotated_projections[b_idx, k_idx, :, a_idx] = np.dot(
+            inv_similarity_matrix[k_idx], r1
+        )[_select_d_order]
+
+    return np.abs(rotated_projections)
+
+
+def get_rotation_masks(projections):
+    nprojections, natoms = projections[Spin.up].shape[2:]
+    mask = np.zeros((3, nprojections))
+
+    if nprojections >= 4:
+        mask[:, 1:4] = _p_mask
+
+    if nprojections >= 9:
+        mask[:, 4:9] = _d_mask
+
+    if nprojections > 9:
+        # rotate f-orbitals?
+        mask[:, 9:] = 1
+
+    return np.tile(mask, natoms)
+
+
+def cosine(v1, v2):
+    # v2 can be list of vectors or single vector
+
+    return_single = False
+    if len(v2.shape) == 1:
+        v2 = v2[None, :]
+        return_single = True
+
+    v1_norm = np.linalg.norm(v1)
+    v2_norm = np.linalg.norm(v2, axis=1)
+
+    v_dot = np.dot(v1, v2.T)
+    v_angle = v_dot / (v1_norm * v2_norm)
+    v_angle[np.isnan(v_angle)] = 1
+
+    if return_single:
+        return v_angle[0]
+    else:
+        return v_angle

amset/kpoints.py
@@ -407,6 +407,7 @@ def expand_kpoints(
     kpoints = kpoints_to_first_bz(kpoints)
     full_kpoints = []
     reduced_to_full_idx = []
+    rot_mapping = []
     equiv_points_mapping = {}
 
     for i, kpoint in enumerate(kpoints):
@@ -418,13 +419,15 @@ def expand_kpoints(
         for ui in unique_idxs:
             spoint = symmetrized_kpoints[ui]
             spoint_round = symmetrized_round_kpoints[ui]
+            rotation_matrix = rotation_matrices[ui]
 
             if spoint_round not in equiv_points_mapping:
                 full_kpoints.append(spoint)
                 equiv_points_mapping[spoint_round] = i
                 reduced_to_full_idx.append(i)
+                rot_mapping.append(rotation_matrix)
 
-    return np.array(full_kpoints), np.array(reduced_to_full_idx)
+    return np.array(full_kpoints), np.array(reduced_to_full_idx), np.array(rot_mapping)
 
 
 def get_mesh_dim_from_kpoints(kpoints, tol=_KTOL):

amset/scattering/calculate.py
@@ -235,14 +235,6 @@ class ScatteringCalculator(object):
         )
         fill_mask = mask[self.amset_data.ir_to_full_kpoint_mapping]
 
-        print(
-            "mean diff",
-            np.mean(
-                band_energies[self.amset_data.ir_to_full_kpoint_mapping]
-                - self.amset_data.energies[spin][b_idx]
-            ),
-        )
-
         logger.debug(
             "  ├── # k-points within Fermi–Dirac cut-offs: {}".format(
                 np.sum(~fill_mask)
@@ -339,37 +331,23 @@ class ScatteringCalculator(object):
             energy,
             return_contributions=True,
             symmetry_reduce=False,
-            band_idx=b_idx,
+            # band_idx=b_idx,
         )
 
         if len(tet_dos) == 0:
             return 0
 
         # next, get k-point indices and band_indices
-        property_mask, band_kpoint_mask, kpoint_mask = tbs.get_masks(spin, tet_mask)
+        property_mask, band_kpoint_mask, band_mask, kpoint_mask = tbs.get_masks(
+            spin, tet_mask
+        )
 
         k = self.amset_data.ir_kpoints[k_idx]
-        k_norm = self.amset_data.kpoint_norms[ir_kpoints_idx][k_idx]
-
         k_primes = self.amset_data.full_kpoints[kpoint_mask]
-        k_primes_norm = self.amset_data.kpoint_norms[kpoint_mask]
 
-        # TODO: how does this work with zone boundary??
-        # k_primes should be something like pbc_diff(k, k_primes) + k
-        # k_primes = pbc_diff(k_primes, k) + k
-        k_dot = np.dot(k, k_primes.T)
-
-        # k_angles is the cosine of the angles
-        k_angles = k_dot / (k_norm * k_primes_norm)
-        k_angles[np.isnan(k_angles)] = 1.0
-
-        a_factor = self.amset_data.a_factor[spin]
-        c_factor = self.amset_data.c_factor[spin]
-
-        a_vals = a_factor[b_idx, ir_kpoints_idx][k_idx] * a_factor[band_kpoint_mask]
-        c_vals = c_factor[b_idx, ir_kpoints_idx][k_idx] * c_factor[band_kpoint_mask]
-
-        overlap = (a_vals + c_vals * k_angles) ** 2
+        overlap = self.amset_data.overlap_calculator.get_overlap(
+            spin, b_idx, k, band_mask, k_primes
+        )
 
         # put overlap back in array with shape (nbands, nkpoints)
         all_overlap = np.zeros(self.amset_data.energies[spin].shape)
@@ -423,6 +401,7 @@ class ScatteringCalculator(object):
                 for f in functions
             ]
         )
+        print("rates", np.max(rates))
 
         # sometimes the projected intersections can be nan when the density of states
         # contribution is infinitesimally small; this catches those errors
@@ -432,7 +411,6 @@ class ScatteringCalculator(object):
         rates *= tet_overlap
 
         return np.sum(rates, axis=-1)
-
 
 #     def calculate_band_rates(self, spin: Spin, b_idx: int, nsplits: int):
 #         integral_conversion = (