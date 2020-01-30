ecb4bde60274fb566da535cc2f6dfbaa561a6deb
Alex Ganose <alexganose@googlemail.com>
2020-01-30 10:15:19 -0800

    MAINT: Add filter deprecation warnings in cli

--- amset/tools/cli.py
+++ amset/tools/cli.py
@@ -1,8 +1,10 @@
 """
 This module contains a script for using amset from the command line.
 """
+import warnings
 
 import click
+from ruamel.yaml.error import MantissaNoDotYAML1_1Warning
 
 from amset.tools.plot import plot
 from amset.tools.run import run
@@ -11,6 +13,12 @@ __author__ = "Alex Ganose"
 __maintainer__ = "Alex Ganose"
 __email__ = "aganose@lbl.gov"
 __date__ = "June 21, 2019"
+
+warnings.filterwarnings("ignore", category=UserWarning, module="pymatgen")
+warnings.filterwarnings("ignore", category=RuntimeWarning, module="amset")
+warnings.filterwarnings("ignore", category=FutureWarning, module="scipy")
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+warnings.simplefilter("ignore", MantissaNoDotYAML1_1Warning)
 
 
 @click.group(context_settings=dict(help_option_names=["-h", "--help"]))
