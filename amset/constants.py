6ed6cf1c4bae6d96744c64f06111429a4fbccd22
Alex Ganose <alexganose@googlemail.com>
2020-02-14 11:15:12 -0800

    Update amset constants

--- amset/constants.py
+++ amset/constants.py
@@ -5,17 +5,18 @@ from scipy.constants import physical_constants
 
 from pymatgen import Spin
 
-bohr_si = physical_constants["Bohr radius"][0]
+bohr_to_m = physical_constants["Bohr radius"][0]
 hartree_si = physical_constants["Hartree energy"][0]
 hbar = physical_constants["Planck constant over 2 pi in eV s"][0]
 hartree_to_ev = physical_constants["Hartree energy in eV"][0]
 ev_to_hartree = 1 / hartree_to_ev
-bohr_to_cm = bohr_si * 100
-cm_to_bohr = 1 / (bohr_si * 100)
-bohr_to_angstrom = bohr_si * 1e10
+bohr_to_cm = bohr_to_m * 100
+cm_to_bohr = 1 / (bohr_to_m * 100)
+bohr_to_angstrom = bohr_to_m * 1e10
 angstrom_to_bohr = 1 / bohr_to_angstrom
-nm_to_bohr = 1 / (bohr_si * 1e9)
-gpa_to_au = bohr_si ** 3 / (1e-9 * hartree_si)
+bohr_to_nm = bohr_to_m * 1e9
+nm_to_bohr = 1 / (bohr_to_m * 1e9)
+gpa_to_au = bohr_to_m ** 3 / (1e-9 * hartree_si)
 e = physical_constants["elementary charge"][0]
 k_B = physical_constants["Boltzmann constant in eV/K"][0]
 small_val = 1e-32  # e.g. used for an initial non-zero val
