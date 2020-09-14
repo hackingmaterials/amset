e5414d8bceef25c7eab2600ffbbb789717369c28
Alex Ganose <alexganose@googlemail.com>
2020-09-14 11:45:33 -0700

    Fix eff-mass

--- amset/tools/effmass.py
+++ amset/tools/effmass.py
@@ -51,6 +51,7 @@ def eff_mass(filename, **kwargs):
     settings = {
         "scattering_type": ["CRT"],
         "constant_relaxation_time": 1e-14,
+        "use_projections": True,
         "calculate_mobility": False,
         "separate_mobility": False,
         "write_log": False,
