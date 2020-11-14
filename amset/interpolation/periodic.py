0d4fb952977f8e3f8106d8407c10c9c8306d692b
Alex Ganose <alexganose@googlemail.com>
2020-11-14 12:57:38 -0800

    Rework periodic interpolator for multiprocessing

--- amset/interpolation/periodic.py
+++ amset/interpolation/periodic.py
@@ -1,7 +1,10 @@
 import logging
+from interpolation.splines import UCGrid
 
 import numpy as np
-from scipy.interpolate import RegularGridInterpolator
+from interpolation.splines import eval_linear
+from interpolation.splines import extrap_options as xto
+
 from scipy.ndimage import gaussian_filter
 
 from amset.constants import numeric_types
@@ -10,83 +13,114 @@ from amset.electronic_structure.kpoints import (
     kpoints_to_first_bz,
 )
 
-try:
-    import interpolation
-except ImportError:
-    interpolation = None
-
 __author__ = "Alex Ganose"
 __maintainer__ = "Alex Ganose"
 __email__ = "aganose@lbl.gov"
+
+from amset.util import create_shared_array
 
 logger = logging.getLogger(__name__)
 
 
 class PeriodicLinearInterpolator(object):
-    def __init__(self, kpoints, data, gaussian=None):
-        self._gaussian = gaussian
-        grid_kpoints, mesh_dim, sort_idx = self._grid_kpoints(kpoints)
-        self._setup_interpolators(data, grid_kpoints, mesh_dim, sort_idx)
+    def __init__(self, nbands, data_shape, interpolators, *args):
+        self.nbands = nbands
+        self.interpolators = interpolators
+        self.data_shape = data_shape
 
-    def _setup_interpolators(self, data, grid_kpoints, mesh_dim, sort_idx):
+    @classmethod
+    def from_data(cls, kpoints, data, gaussian=None):
+        grid_kpoints, mesh_dim, sort_idx = cls._grid_kpoints(kpoints)
+        nbands, data_shape, interpolators = cls._setup_interpolators(
+            data, grid_kpoints, mesh_dim, sort_idx, gaussian
+        )
+        return cls(nbands, data_shape, interpolators)
+
+    def to_reference(self):
+        interpolator_references = self._interpolators_to_reference()
+        return self.nbands, self.data_shape, interpolator_references
+
+    def _interpolators_to_reference(self):
+        interpolator_references = {}
+        for spin, (grid, grid_data) in self.interpolators.items():
+            grid_buffer, grid_shared = create_shared_array(
+                grid, return_shared_data=True
+            )
+            grid_data_buffer, grid_data_shared = create_shared_array(
+                grid_data, return_shared_data=True
+            )
+            self.interpolators[spin] = (grid_shared, grid_data_shared)
+            interpolator_references[spin] = (
+                grid_buffer,
+                grid.shape,
+                grid_data_buffer,
+                grid_data.shape,
+            )
+        return interpolator_references
+
+    @classmethod
+    def from_reference(cls, nbands, data_shape, interpolator_references, *args):
+        interpolators = cls._interpolators_from_reference(interpolator_references)
+        return cls(nbands, data_shape, interpolators, *args)
+
+    @staticmethod
+    def _interpolators_from_reference(interpolator_references):
+        interpolators = {}
+        for (
+            spin,
+            (grid_buffer, grid_shape, grid_data_buffer, grid_data_shape),
+        ) in interpolator_references.items():
+            grid = np.frombuffer(grid_buffer).reshape(grid_shape)
+            grid_data = np.frombuffer(grid_data_buffer).reshape(grid_data_shape)
+            interpolators[spin] = (grid, grid_data)
+        return interpolators
+
+    @staticmethod
+    def _setup_interpolators(data, grid_kpoints, mesh_dim, sort_idx, gaussian):
         x = grid_kpoints[:, 0, 0, 0]
         y = grid_kpoints[0, :, 0, 1]
         z = grid_kpoints[0, 0, :, 2]
 
-        self.nbands = {s: c.shape[0] for s, c in data.items()}
-        self.interpolators = {}
+        nbands = {s: c.shape[0] for s, c in data.items()}
+        interpolators = {}
+        data_shape = None
         for spin, spin_data in data.items():
             data_shape = spin_data.shape[2:]
-            nbands = self.nbands[spin]
-            self.data_shape = data_shape
+            spin_nbands = nbands[spin]
 
             # sort the data then reshape them into the grid. The data
             # can now be indexed as data[iband][ikx][iky][ikz]
             sorted_data = spin_data[:, sort_idx]
-            grid_shape = (nbands,) + mesh_dim + data_shape
+            grid_shape = (spin_nbands,) + mesh_dim + data_shape
             grid_data = sorted_data.reshape(grid_shape)
 
             # wrap the data to account for PBC
             pad_size = ((0, 0), (1, 1), (1, 1), (1, 1)) + ((0, 0),) * len(data_shape)
             grid_data = np.pad(grid_data, pad_size, mode="wrap")
 
-            if self._gaussian:
+            if gaussian:
                 for i in range(len(grid_data)):
                     grid_data[i] = gaussian_filter(
-                        grid_data[i], sigma=self._gaussian, mode="wrap"
+                        grid_data[i], sigma=gaussian, mode="wrap"
                     )
 
-            if nbands == 1:
+            if spin_nbands == 1:
                 # this can cause a bug in RegularGridInterpolator. Have to fake
                 # having at least two bands
-                nbands = 2
+                spin_nbands = 2
                 grid_data = np.tile(grid_data, (2, 1, 1, 1) + (1,) * len(data_shape))
 
-            if interpolation is not None:
-                from interpolation.splines import UCGrid
+            grid = UCGrid(
+                (0, spin_nbands - 1, spin_nbands),
+                (x[0], x[-1], len(x)),
+                (y[0], y[-1], len(y)),
+                (z[0], z[-1], len(z)),
+            )
+            # flatten remaining axes
+            grid_shape = grid_data.shape[:4] + (-1,)
+            interpolators[spin] = (grid, grid_data.reshape(grid_shape))
 
-                grid = UCGrid(
-                    (0, nbands - 1, nbands),
-                    (x[0], x[-1], len(x)),
-                    (y[0], y[-1], len(y)),
-                    (z[0], z[-1], len(z)),
-                )
-                # flatten remaining axes
-                grid_shape = grid_data.shape[:4] + (-1,)
-                self.interpolators[spin] = (grid, grid_data.reshape(grid_shape))
-            else:
-                logger.warning(
-                    "Install the 'interpolation' package for improved performance: "
-                    "https://pypi.org/project/interpolation"
-                )
-                interp_range = (np.arange(nbands), x, y, z)
-
-                self.interpolators[spin] = RegularGridInterpolator(
-                    interp_range,
-                    grid_data,
-                    bounds_error=False,
-                    fill_value=None,  # , method="nearest"
-                )
+        return nbands, data_shape, interpolators
 
     @staticmethod
     def _grid_kpoints(kpoints):
@@ -120,26 +154,20 @@ class PeriodicLinearInterpolator(object):
     def interpolate(self, spin, bands, kpoints):
         v = np.concatenate([np.asarray(bands)[:, None], np.asarray(kpoints)], axis=1)
 
-        if interpolation:
-            from interpolation.splines import eval_linear
-            from interpolation.splines import extrap_options as xto
-
-            grid, data = self.interpolators[spin]
-            if np.iscomplexobj(data):
-                # only allows interpolating floats, so have to separate real and imag
-                interp_data = np.empty((len(v),) + self.data_shape, dtype=np.complex)
-                interp_data.real = eval_linear(grid, data.real, v, xto.LINEAR).reshape(
-                    -1, *self.data_shape
-                )
-                interp_data.imag = eval_linear(grid, data.imag, v, xto.LINEAR).reshape(
-                    -1, *self.data_shape
-                )
-            else:
-                interp_data = eval_linear(grid, data, v, xto.LINEAR).reshape(
-                    -1, *self.data_shape
-                )
+        grid, data = self.interpolators[spin]
+        if np.iscomplexobj(data):
+            # only allows interpolating floats, so have to separate real and imag
+            interp_data = np.empty((len(v),) + self.data_shape, dtype=np.complex)
+            interp_data.real = eval_linear(grid, data.real, v, xto.LINEAR).reshape(
+                -1, *self.data_shape
+            )
+            interp_data.imag = eval_linear(grid, data.imag, v, xto.LINEAR).reshape(
+                -1, *self.data_shape
+            )
         else:
-            interp_data = self.interpolators[spin](v)
+            interp_data = eval_linear(grid, data, v, xto.LINEAR).reshape(
+                -1, *self.data_shape
+            )
 
         return interp_data
 
