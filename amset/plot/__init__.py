68c53a31c3aae5c040a6937567f52c94151eb9fc
Alex Ganose <alexganose@googlemail.com>
2020-11-20 18:23:42 -0800

    Add styled subplots

--- amset/plot/__init__.py
+++ amset/plot/__init__.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot
+from matplotlib import rcParams
 
 from amset.plot.base import (  # noqa
     BaseMeshPlotter,
@@ -52,3 +53,72 @@ def styled_plot(*style_sheets):
         return wrapper
 
     return decorator
+
+
+def pretty_subplot(
+    nrows,
+    ncols,
+    width=None,
+    height=None,
+    sharex=False,
+    sharey=False,
+    dpi=None,
+    plt=None,
+    gridspec_kw=None,
+):
+    """Get a :obj:`matplotlib.pyplot` subplot object with pretty defaults.
+
+    Args:
+        nrows (int): The number of rows in the subplot.
+        ncols (int): The number of columns in the subplot.
+        width (:obj:`float`, optional): The width of the plot.
+        height (:obj:`float`, optional): The height of the plot.
+        sharex (:obj:`bool`, optional): All subplots share the same x-axis.
+            Defaults to ``True``.
+        sharey (:obj:`bool`, optional): All subplots share the same y-axis.
+            Defaults to ``True``.
+        dpi (:obj:`int`, optional): The dots-per-inch (pixel density) for
+            the plot.
+        plt (:obj:`matplotlib.pyplot`, optional): A :obj:`matplotlib.pyplot`
+            object to use for plotting.
+        gridspec_kw (:obj:`dict`, optional): Gridspec parameters. Please see:
+            :obj:`matplotlib.pyplot.subplot` for more information. Defaults
+            to ``None``.
+
+    Returns:
+        :obj:`matplotlib.pyplot`: A :obj:`matplotlib.pyplot` subplot object
+        with publication ready defaults set.
+    """
+
+    if width is None:
+        width = rcParams["figure.figsize"][0]
+    if height is None:
+        height = rcParams["figure.figsize"][1]
+
+    # TODO: Make this work if plt is already set...
+    if plt is None:
+        plt = matplotlib.pyplot
+        fig, axes = plt.subplots(
+            nrows,
+            ncols,
+            sharex=sharex,
+            sharey=sharey,
+            dpi=dpi,
+            figsize=(width, height),
+            facecolor="w",
+            gridspec_kw=gridspec_kw,
+        )
+
+    return fig, axes
+
+
+def get_figsize(nrows, ncols, width=None, height=None, wspace=0.4, hspace=0.3):
+    if width is None:
+        width = matplotlib.rcParams["figure.figsize"][0]
+        width = width * ncols + width * wspace * (ncols - 1)
+
+    if height is None:
+        height = matplotlib.rcParams["figure.figsize"][1]
+        height = height * nrows + height * hspace * (nrows - 1)
+
+    return width, height
